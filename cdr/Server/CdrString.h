2831b01453f2fede9f155b6d5b56ec0568328270
Bob Kline <bkline@rksystems.com>
2000-04-14 16:02:00 +0000

    <R24> Added support for converting UTF-8 to UTF-16.

--- cdr/Server/CdrString.h
+++ cdr/Server/CdrString.h
@@ -1,7 +1,10 @@
 /*
- * $Id: CdrString.h,v 1.1 2000-04-11 14:18:24 bkline Exp $
+ * $Id: CdrString.h,v 1.2 2000-04-14 16:02:00 bkline Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/04/11 14:18:24  bkline
+ * Initial revision
+ *
  */
 
 #ifndef CDR_STRING_
@@ -28,10 +31,15 @@ namespace cdr {
     public:
         String() {}
         String(const wchar_t* s) : StdWstring(s) {}
+        String(const wchar_t* s, size_t n) : StdWstring(s, n) {}
         String(const DOMString& s) : StdWstring(s.rawBuffer(), s.length()) {}
         String(const StdWstring& s) : StdWstring(s) {}
+        String(const char* s) { utf8ToUtf16(s); }
+        String(const std::string& s) { utf8ToUtf16(s.c_str()); }
         int getInt() const;
         std::string toUtf8() const;
+    private:
+        void utf8ToUtf16(const char*);
     };
 
     // Containers of our strings.
