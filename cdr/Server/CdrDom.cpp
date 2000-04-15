9f9ec83064229de45bafd2e13969a7975547148c
Bob Kline <bkline@rksystems.com>
2000-04-15 14:06:39 +0000

    <R30> Added getTextContent function.

--- cdr/Server/CdrDom.cpp
+++ cdr/Server/CdrDom.cpp
@@ -1,7 +1,10 @@
 /*
- * $Id: CdrDom.cpp,v 1.1 2000-04-11 14:16:41 bkline Exp $
+ * $Id: CdrDom.cpp,v 1.2 2000-04-15 14:06:39 bkline Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/04/11 14:16:41  bkline
+ * Initial revision
+ *
  */
 
 #include "CdrDom.h"
@@ -18,4 +21,16 @@ void cdr::dom::Parser::parse(const std::string& xml)
 {
     MemBufInputSource s((const XMLByte* const)xml.c_str(), xml.size(), "MEM");
     ((::DOMParser *)this)->parse(s);
+}
+
+cdr::String cdr::dom::getTextContent(const cdr::dom::Node& node)
+{
+    cdr::String str;
+    cdr::dom::Node child = node.getFirstChild();
+    while (child != 0) {
+        if (child.getNodeType() == cdr::dom::Node::TEXT_NODE)
+            str += cdr::String(child.getNodeValue());
+        child = child.getNextSibling();
+    }
+    return str;
 }
