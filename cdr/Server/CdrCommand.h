e99b6f4be2ab75f8b173a31c69600445da8fea11
Bob Kline <bkline@rksystems.com>
2000-04-15 12:05:34 +0000

    <R25> Changed DbConnection* to DbConnection&.  Removed redundant namespace qualifiers.

--- cdr/Server/CdrCommand.h
+++ cdr/Server/CdrCommand.h
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrCommand.h,v 1.1 2000-04-14 15:58:04 bkline Exp $
+ * $Id: CdrCommand.h,v 1.2 2000-04-15 12:05:34 bkline Exp $
  *
  * Interface for CDR command handlers.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/04/14 15:58:04  bkline
+ * Initial revision
+ *
  */
 
 #ifndef CDR_COMMAND_
@@ -15,58 +18,32 @@
 #include "CdrDbConnection.h"
 
 namespace cdr {
-    typedef cdr::String (*Command)(cdr::Session&, 
-                                   const cdr::dom::Node&,
-                                   cdr::DbConnection*);
-    extern Command lookupCommand(const cdr::String&);
-    extern cdr::String logon(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String logoff(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String checkAuth(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String addGrp(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String getGrp(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String modGrp(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String delGrp(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String listGrps(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String addUsr(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String getUsr(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String modUsr(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String delUsr(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String listUsrs(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String addDoc(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String repDoc(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String delDoc(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String validateDoc(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String search(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String getDoc(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String checkOut(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String checkIn(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String report(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String filter(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
-    extern cdr::String getLinks(cdr::Session&, 
-        const cdr::dom::Node&, cdr::DbConnection*);
+    typedef String (*Command)(Session&, dom::Node&, db::Connection&);
+    extern Command lookupCommand(const String&);
+    extern String logon      (Session&, const dom::Node&, db::Connection&);
+    extern String logoff     (Session&, const dom::Node&, db::Connection&);
+    extern String checkAuth  (Session&, const dom::Node&, db::Connection&);
+    extern String addGrp     (Session&, const dom::Node&, db::Connection&);
+    extern String getGrp     (Session&, const dom::Node&, db::Connection&);
+    extern String modGrp     (Session&, const dom::Node&, db::Connection&);
+    extern String delGrp     (Session&, const dom::Node&, db::Connection&);
+    extern String listGrps   (Session&, const dom::Node&, db::Connection&);
+    extern String addUsr     (Session&, const dom::Node&, db::Connection&);
+    extern String getUsr     (Session&, const dom::Node&, db::Connection&);
+    extern String modUsr     (Session&, const dom::Node&, db::Connection&);
+    extern String delUsr     (Session&, const dom::Node&, db::Connection&);
+    extern String listUsrs   (Session&, const dom::Node&, db::Connection&);
+    extern String addDoc     (Session&, const dom::Node&, db::Connection&);
+    extern String repDoc     (Session&, const dom::Node&, db::Connection&);
+    extern String delDoc     (Session&, const dom::Node&, db::Connection&);
+    extern String validateDoc(Session&, const dom::Node&, db::Connection&);
+    extern String search     (Session&, const dom::Node&, db::Connection&);
+    extern String getDoc     (Session&, const dom::Node&, db::Connection&);
+    extern String checkOut   (Session&, const dom::Node&, db::Connection&);
+    extern String checkIn    (Session&, const dom::Node&, db::Connection&);
+    extern String report     (Session&, const dom::Node&, db::Connection&);
+    extern String filter     (Session&, const dom::Node&, db::Connection&);
+    extern String getLinks   (Session&, const dom::Node&, db::Connection&);
 }
 
 #endif
