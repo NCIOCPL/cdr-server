4a341c36a04d608f509b626ab9570dfd18eafa5d
Bob Kline <bkline@rksystems.com>
2000-04-16 21:37:25 +0000

    <R38> Changed member names, added new member variables and methods.

--- cdr/Server/CdrSession.h
+++ cdr/Server/CdrSession.h
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrSession.h,v 1.2 2000-04-15 12:15:37 bkline Exp $
+ * $Id: CdrSession.h,v 1.3 2000-04-16 21:37:25 bkline Exp $
  *
  * Information about the current login.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2000/04/15 12:15:37  bkline
+ * Added uName and uid members.
+ *
  * Revision 1.1  2000/04/14 16:01:08  bkline
  * Initial revision
  *
@@ -13,12 +16,17 @@
 #define CDR_SESSION_
 
 #include "CdrString.h"
+#include "CdrDbConnection.h"
 
 namespace cdr {
     struct Session {
-        cdr::String id;
-        cdr::String uName;
+        int id;
+        String name;
         int uid;
+        String uName;
+        String lastStatus;
+        void lookupSession(const String&, db::Connection&);
+        void setLastActivity(db::Connection&) const;
     };
 }
 
