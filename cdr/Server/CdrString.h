7180c9ca5960b89e12b52c4808f039ed16fd8c0c
Bob Kline <bkline@rksystems.com>
2000-04-17 21:28:01 +0000

    <R54> Made cdr::String nullable.

--- cdr/Server/CdrString.h
+++ cdr/Server/CdrString.h
@@ -1,7 +1,10 @@
 /*
- * $Id: CdrString.h,v 1.2 2000-04-14 16:02:00 bkline Exp $
+ * $Id: CdrString.h,v 1.3 2000-04-17 21:28:01 bkline Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2000/04/14 16:02:00  bkline
+ * Added support for converting UTF-8 to UTF-16.
+ *
  * Revision 1.1  2000/04/11 14:18:24  bkline
  * Initial revision
  *
@@ -29,17 +32,20 @@ namespace cdr {
      */
     class String : public StdWstring {
     public:
-        String() {}
-        String(const wchar_t* s) : StdWstring(s) {}
-        String(const wchar_t* s, size_t n) : StdWstring(s, n) {}
-        String(const DOMString& s) : StdWstring(s.rawBuffer(), s.length()) {}
-        String(const StdWstring& s) : StdWstring(s) {}
-        String(const char* s) { utf8ToUtf16(s); }
-        String(const std::string& s) { utf8ToUtf16(s.c_str()); }
+        String(bool n = false) : null(n) {}
+        String(const wchar_t* s) : StdWstring(s), null(false) {}
+        String(const wchar_t* s, size_t n) : StdWstring(s, n), null(false) {}
+        String(const StdWstring& s) : StdWstring(s), null(false) {}
+        String(const char* s) : null(false) { utf8ToUtf16(s); }
+        String(const std::string& s) : null(false) { utf8ToUtf16(s.c_str()); }
+        String(const DOMString& s) 
+            : StdWstring(s.rawBuffer(), s.length()), null(false) {}
+        bool isNull() const { return null; }
         int getInt() const;
         std::string toUtf8() const;
     private:
         void utf8ToUtf16(const char*);
+        bool null;
     };
 
     // Containers of our strings.
