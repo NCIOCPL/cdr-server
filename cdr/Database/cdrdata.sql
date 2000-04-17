0261066e24f1fc86ab1b9df9c3ff249ff5df41a9
Bob Kline <bkline@rksystems.com>
2000-04-17 21:34:02 +0000

    <R55> Data mods to test CdrCheckAuth command.

--- cdr/Database/cdrdata.sql
+++ cdr/Database/cdrdata.sql
@@ -1,7 +1,10 @@
 /*
- * $Id: cdrdata.sql,v 1.1 2000-04-14 14:04:58 bkline Exp $
+ * $Id: cdrdata.sql,v 1.2 2000-04-17 21:34:02 bkline Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/04/14 14:04:58  bkline
+ * Initial revision
+ *
  */
 
 INSERT INTO usr(name, password, created) VALUES('mmr', '***REDACTED***', GETDATE())
@@ -9,6 +12,7 @@ INSERT INTO usr(name, password, created) VALUES('rmk', '***REDACTED***', GETDATE
 INSERT INTO usr(name, password, created) VALUES('ahm', '***REDACTED***', GETDATE())
 go
 
+INSERT INTO format(name) VALUES('none')
 INSERT INTO format(name) VALUES('xml')
 INSERT INTO format(name) VALUES('html')
 INSERT INTO format(name) VALUES('worddoc')
@@ -17,6 +21,8 @@ INSERT INTO format(name) VALUES('pgn')
 INSERT INTO format(name) VALUES('smtp')
 go
 
+INSERT INTO doc_type(name, format, created, versioning, dtd, xml_schema)
+VALUES('', 2, GETDATE(), 'N', '', '')
 INSERT INTO doc_type(name, format, created, versioning, dtd, xml_schema)
 VALUES('Person', 1, GETDATE(), 'Y',
        '<!ELEMENT Person     (FirstName, LastName)>\n' +
@@ -27,6 +33,16 @@ VALUES('Person', 1, GETDATE(), 'Y',
        '    <xsd:element name="FirstName" type="xsd:string"/>\n' +
        '    <xsd:element name="LastName"  type="xsd:string"/>\n' +
        '</xsd:complexType>')
+INSERT INTO doc_type(name, format, created, versioning, dtd, xml_schema)
+VALUES('Org', 2, GETDATE(), 'Y',
+       '<!ELEMENT Org        (Name, Address)>\n' +
+       '<!ELEMENT Name       (#PCDATA)>\n' +
+       '<!ELEMENT Address    (#PCDATA)>\n',
+       '<xsd:element     name="Org"       type="Org"/>\n' +
+       '<xsd:complexType name="Org">\n' +
+       '    <xsd:element name="Name"      type="xsd:string"/>\n' +
+       '    <xsd:element name="Address"   type="xsd:string"/>\n' +
+       '</xsd:complexType>')
 go
 
 INSERT INTO doc_status(id, name) VALUES('U', 'UNVALIDATED')
@@ -36,16 +52,16 @@ INSERT INTO doc_status(id, name) VALUES('P', 'PUBLISHED')
 go
 
 INSERT INTO document(created, creator, doc_type, title, xml)
-VALUES(GETDATE(), 1, 1, 'James Bond', 
+VALUES(GETDATE(), 1, 2, 'James Bond', 
   '<Person><FirstName>James</FirstName><LastName>Bond</LastName></Person>')
 INSERT INTO document(created, creator, doc_type, title, xml)
-VALUES(GETDATE(), 2, 1, 'Sam Spade', 
+VALUES(GETDATE(), 2, 2, 'Sam Spade', 
   '<Person><FirstName>Sam</FirstName><LastName>Spade</LastName></Person>')
 INSERT INTO document(created, creator, doc_type, title, xml)
-VALUES(GETDATE(), 3, 1, 'George Smiley', 
+VALUES(GETDATE(), 3, 2, 'George Smiley', 
   '<Person><FirstName>George</FirstName><LastName>Smiley</LastName></Person>')
 INSERT INTO document(created, creator, doc_type, title, xml)
-VALUES(GETDATE(), 1, 1, 'Sherlock Holmes', 
+VALUES(GETDATE(), 1, 2, 'Sherlock Holmes', 
   '<Person><FirstName>Sherlock</FirstName><LastName>Holmes</LastName></Person>')
 go
 
@@ -60,26 +76,37 @@ INSERT INTO action(name) VALUES('MODIFY DOCUMENT')
 INSERT INTO action(name) VALUES('DELETE DOCUMENT')
 INSERT INTO action(name) VALUES('APPROVE DOCUMENT')
 INSERT INTO action(name) VALUES('PUBLISH DOCUMENT')
+INSERT INTO action(name) VALUES('CREATE USER')
+INSERT INTO action(name) VALUES('DELETE USER')
+INSERT INTO action(name) VALUES('MODIFY USER')
 go
 
 INSERT INTO grp(name) VALUES('TRAINEES')
 INSERT INTO grp(name) VALUES('EDITORS')
 INSERT INTO grp(name) VALUES('MANAGERS')
+INSERT INTO grp(name) VALUES('ORG EDITORS')
 go
 
-INSERT INTO grp_action(grp, action, doc_type) VALUES(1, 1, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(1, 2, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(2, 1, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(2, 2, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(2, 3, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 1, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 2, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 3, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 4, 1)
-INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 5, 1)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(1, 1, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(1, 2, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(2, 1, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(2, 2, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(2, 3, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 1, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 2, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 3, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 4, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 5, 2)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 6, 1)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 7, 1)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(3, 8, 1)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(4, 1, 3)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(4, 2, 3)
+INSERT INTO grp_action(grp, action, doc_type) VALUES(4, 3, 3)
 go
 
 INSERT INTO grp_usr(grp, usr) VALUES(1, 2)
 INSERT INTO grp_usr(grp, usr) VALUES(2, 1)
 INSERT INTO grp_usr(grp, usr) VALUES(1, 3)
+INSERT INTO grp_usr(grp, usr) VALUES(4, 3)
 go
