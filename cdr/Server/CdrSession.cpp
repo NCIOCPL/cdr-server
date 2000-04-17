b23468c4dc6d9064f2317a9f941698d4728e2aef
Bob Kline <bkline@rksystems.com>
2000-04-17 03:10:21 +0000

    <R46> Fixed typo in wide string constant.

--- cdr/Server/CdrSession.cpp
+++ cdr/Server/CdrSession.cpp
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrSession.cpp,v 1.1 2000-04-16 21:38:32 bkline Exp $
+ * $Id: CdrSession.cpp,v 1.2 2000-04-17 03:10:21 bkline Exp $
  *
  * Session control information.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/04/16 21:38:32  bkline
+ * Initial revision
+ *
  */
 
 #include "CdrSession.h"
@@ -18,13 +21,13 @@ void cdr::Session::lookupSession(const cdr::String& sessionId,
     cdr::db::ResultSet rs = select.executeQuery("SELECT s.id,"
                                                 "       s.usr,"
                                                 "       u.name"
-                                                "  FROM session s,"
-                                                "       usr     u"
+                                                "  FROM session s"
+                                                "  JOIN usr     u"
+                                                "    ON s.usr   = u.id"
                                                 " WHERE s.name  = ?"
-                                                "   AND s.usr   = u.id"
                                                 "   AND s.ended IS NULL");
     if (!rs.next())
-        throw cdr::Exception("LInvalid or expired session", sessionId);
+        throw cdr::Exception(L"Invalid or expired session", sessionId);
     name  = sessionId;
     id    = rs.getInt(1);
     uid   = rs.getInt(2);
