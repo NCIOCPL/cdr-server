1f8fdd1c3b91d4aae55a4ac79ad4c45d8ff4a385
Bob Kline <bkline@rksystems.com>
2000-04-21 14:02:06 +0000

    <R63> Added canDo() method.

--- cdr/Server/CdrSession.cpp
+++ cdr/Server/CdrSession.cpp
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrSession.cpp,v 1.2 2000-04-17 03:10:21 bkline Exp $
+ * $Id: CdrSession.cpp,v 1.3 2000-04-21 13:59:00 bkline Exp $
  *
  * Session control information.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2000/04/17 03:10:21  bkline
+ * Fixed typo in wide string constant.
+ *
  * Revision 1.1  2000/04/16 21:38:32  bkline
  * Initial revision
  *
@@ -41,4 +44,30 @@ void cdr::Session::setLastActivity(db::Connection& conn) const
     update.executeQuery("UPDATE session"
                         "   SET last_act = GETDATE()"
                         " WHERE id = ?");
+}
+
+bool cdr::Session::canDo(db::Connection& conn, 
+                         const cdr::String& action,
+                         const cdr::String& docType) const
+{
+    cdr::db::Statement query(conn);
+    query.setInt(1, id);
+    query.setString(2, action);
+    query.setString(3, docType);
+    cdr::db::ResultSet rs = query.executeQuery("SELECT COUNT(*)"
+                                               "  FROM grp_usr    gu"
+                                               "       grp_action ga"
+                                               "       action     a"
+                                               "       doc_type   t"
+                                               " WHERE ga.action   = a.id"
+                                               "   AND ga.doc_type = t.id"
+                                               "   AND ga.grp      = gu.grp"
+                                               "   AND gu.usr      = ?"
+                                               "   AND a.name      = ?"
+                                               "   AND t.name      = ?");
+    if (!rs.next())
+        // Shouldn't ever reach here.
+        throw cdr::Exception(L"Database failure extracting result count");
+    int count = rs.getInt(1);
+    return count > 0;
 }
--- cdr/Server/CdrSession.h
+++ cdr/Server/CdrSession.h
@@ -1,15 +1,17 @@
 /*
- * $Id: CdrSession.h,v 1.3 2000-04-16 21:37:25 bkline Exp $
+ * $Id: CdrSession.h,v 1.4 2000-04-21 14:02:06 bkline Exp $
  *
  * Information about the current login.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2000/04/16 21:37:25  bkline
+ * Changed member names, added new member variables and methods.
+ *
  * Revision 1.2  2000/04/15 12:15:37  bkline
  * Added uName and uid members.
  *
  * Revision 1.1  2000/04/14 16:01:08  bkline
  * Initial revision
- *
  */
 
 #ifndef CDR_SESSION_
@@ -27,6 +29,9 @@ namespace cdr {
         String lastStatus;
         void lookupSession(const String&, db::Connection&);
         void setLastActivity(db::Connection&) const;
+        bool canDo(db::Connection&, 
+                   const cdr::String&, 
+                   const cdr::String&) const;
     };
 }
 
