c885e1180a9509523cb61955cb9bd5b1800db29e
Bob Kline <bkline@rksystems.com>
2000-04-22 15:36:15 +0000

    <R78> Filled out documentation comments.  Made all ODBC-specific members private.

--- cdr/Server/CdrDbConnection.h
+++ cdr/Server/CdrDbConnection.h
@@ -1,15 +1,17 @@
 /*
- * $Id: CdrDbConnection.h,v 1.3 2000-04-22 09:35:43 bkline Exp $
+ * $Id: CdrDbConnection.h,v 1.4 2000-04-22 15:35:15 bkline Exp $
  *
  * Interface for CDR wrapper for an ODBC database connection.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2000/04/22 09:35:43  bkline
+ * Added transaction support and getLastIdent() method.
+ *
  * Revision 1.2  2000/04/15 12:08:40  bkline
  * First non-stub version.
  *
  * Revision 1.1  2000/04/14 15:58:42  bkline
  * Initial revision
- *
  */
 
 #ifndef CDR_DB_CONNECTION_
@@ -22,19 +24,55 @@
 
 namespace cdr {
     namespace db {
+
+        // Forward references.
+        class Statement;
+        class ResultSet;
+
+        /**
+         * Implements JDBC-like interface of same name for CDR database
+         * connections.  Also includes a non-standard method for asking 
+         * SQL Server for the current IDENTITY value.
+         */
         class Connection {
+
+            /**
+             * These classes are granted access to Connection's private
+             * members to eliminate the need for public getEnv() and
+             * getDbc() accessor methods.
+             */
+            friend Statement;
+            friend ResultSet;
+
         public:
+
+            /**
+             * These will be made private, with a new Driver friend class,
+             * which will handle connection pooling in its connect() method.
+             */
             Connection();
             ~Connection();
+
+            /**
+             * Turning off auto-commit starts an open transaction,
+             * which can be committed or rolled back.  A new connection
+             * is started with auto-commit turned on, effectively making 
+             * each query into a self-contained transaction, committed
+             * if successful.
+             */
             bool getAutoCommit() const { return autoCommit; }
             void setAutoCommit(bool);
             void commit();
             void rollback();
+
+            /**
+             * Extracts the current @@IDENTITY value from SQL Server for
+             * the current connection.
+             */
             int getLastIdent();
-            HENV getEnv() { return henv; }
-            HDBC getDbc() { return hdbc; }
-            static cdr::String getErrorMessage(SQLRETURN, HENV, HDBC, HSTMT);
+
         private:
+            static cdr::String getErrorMessage(SQLRETURN, HENV, HDBC, HSTMT);
             HDBC hdbc;
             static HENV henv;
             bool autoCommit;
--- cdr/Server/CdrDbStatement.h
+++ cdr/Server/CdrDbStatement.h
@@ -1,15 +1,17 @@
 /*
- * $Id: CdrDbStatement.h,v 1.3 2000-04-22 09:36:24 bkline Exp $
+ * $Id: CdrDbStatement.h,v 1.4 2000-04-22 15:36:15 bkline Exp $
  *
  * Wrapper for ODBC HSTMT.  Modeled after JDBC interface.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2000/04/22 09:36:24  bkline
+ * Added close() method.
+ *
  * Revision 1.2  2000/04/17 21:27:02  bkline
  * Added nullability for ints and strings.
  *
  * Revision 1.1  2000/04/15 12:16:29  bkline
  * Initial revision
- *
  */
 
 #ifndef CDR_DB_STATEMENT_
@@ -21,20 +23,56 @@
 
 namespace cdr {
     namespace db {
+
         class ResultSet;
+
+        /**
+         * Implements JDBC-like interface of same name for queries
+         * submitted to the CDR database.
+         */
         class Statement {
+
+            /**
+             * The ResultSet class is granted access to the private
+             * members of Statement in order to eliminate the need for
+             * public accessor methods for ODBC-specific handles.
+             */
+            friend ResultSet;
+
          public:
             Statement(Connection&);
             ~Statement();
+
+            /**
+             * Registers a wide string parameter for a query.
+             */
             void        setString(int, const cdr::String&);
+
+            /**
+             * Registers a narrow string parameter for a query.
+             *
+             *  @deprecated
+             */
             void        setString(int, const std::string&, bool = false);
+
+            /**
+             * Registers an integer parameter for a query.  Uses
+             * the cdr::Int class, which can represent a NULL value.
+             */
             void        setInt(int, const cdr::Int&);
+
+            /**
+             * Submits a query to the CDR database, and returns a
+             * ResultSet object for retrieving rows, if any.
+             */
             ResultSet   executeQuery(const char*);
+
+            /**
+             * Closes any open cursors associated with the query,
+             * making it available for re-use.
+             */
             void        close();
-            HSTMT       getStmt() const { return hstmt; }
-            HDBC        getDbc() const { return conn.getDbc(); }
-            HENV        getEnv() const { return conn.getEnv(); }
-            cdr::String getErrorMessage(SQLRETURN);
+
         private:
             Connection& conn;
             HSTMT hstmt;
@@ -48,6 +86,8 @@ namespace cdr {
             };
             typedef std::vector<Parameter*> ParamVector;
             ParamVector paramVector;
+            cdr::String getErrorMessage(SQLRETURN);
+            void clearParms();
         };
     }
 }
