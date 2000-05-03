7dfd73c0bc5a64f77c82ac246c675fb09ea302e0
Bob Kline <bkline@rksystems.com>
2000-05-03 17:27:54 +0000

    <R118> Fleshed out ccdoc comments.

--- cdr/Server/CdrBlob.h
+++ cdr/Server/CdrBlob.h
@@ -1,9 +1,11 @@
 /*
- * $Id: CdrBlob.h,v 1.1 2000-05-03 15:37:25 bkline Exp $
+ * $Id: CdrBlob.h,v 1.2 2000-05-03 17:27:54 bkline Exp $
  *
  * CDR wrapper for a string of bytes.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/05/03 15:37:25  bkline
+ * Initial revision
  */
 
 #ifndef CDR_BLOB_
@@ -29,12 +31,46 @@ namespace cdr {
      * Wrapper class for strings of bytes.
      */
     class Blob : public BlobBase {
+
     public:
+
+        /**
+         * Creates an empty (and possibly NULL) object for a string of bytes.
+         *
+         *  @param  null_       <code>true</code> if new object should be
+         *                      <code>NULL</code>; default is
+         *                      <code>false</code>
+         */
         Blob(bool null_ = false) : null(null_) {}
+
+        /**
+         * Creates a new non-nul <code>Blob</code> object from an array 
+         * of bytes.
+         *
+         *  @param  s           address of array of bytes.
+         *  @param  n           number of bytes in array.
+         */
         Blob(const unsigned char*s, size_t n) : BlobBase(s, n), null(false) {}
+
+        /**
+         * Copy constructor.
+         *
+         *  @param  b           reference to object to be copied.
+         */
         Blob(const Blob& b) : BlobBase(b), null(b.null) {}
+
+        /**
+         * Returns setting of object's <code>null</code> flag.
+         *
+         *  @returns            <code>true</code> if object is NULL.
+         */
         bool    isNull() const { return null; }
+
     private:
+
+        /**
+         * Flag remembering whether object is <code>NULL</code>.
+         */
         bool    null;
     };
 
