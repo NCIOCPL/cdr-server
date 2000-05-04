8be96d38cfaa83c5069e5bbfd6567f113f1457b6
Bob Kline <bkline@rksystems.com>
2000-05-04 11:52:35 +0000

    <R133> More ccdoc comments.

--- cdr/Server/CdrParserInput.h
+++ cdr/Server/CdrParserInput.h
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrParserInput.h,v 1.3 2000-04-22 18:57:38 bkline Exp $
+ * $Id: CdrParserInput.h,v 1.4 2000-05-04 11:52:35 bkline Exp $
  *
  * Maintains state information for input for CDR string parser.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2000/04/22 18:57:38  bkline
+ * Added ccdoc comment markers for namespaces and @pkg directives.
+ *
  * Revision 1.2  2000/04/22 16:33:24  bkline
  * Fleshed out documentation.
  *
@@ -34,6 +37,8 @@ namespace cdr {
 
         /**
          * Creates a new input object, reading for parsing.
+         *
+         *  @param  s       reference to string to be parsed.
          */
         ParserInput(const String& s) 
             : str(s), pos(0), buf(str.c_str()), len(str.size()), lastTok(-1) {}
@@ -50,10 +55,15 @@ namespace cdr {
          * by blocking the assignment operator and copy constructor
          * such a misuse has been prevented.
          *
-         *  @throw  cdr::Exception if an attempt is made to increment
-         *                         past the end of the input.
+         *  @param  dummy           unused argument flagging this
+         *                          version of the operator as the
+         *                          post-increment flavor.
+         *  @return                 copy of the object before incrementing.
+         *
+         *  @exception cdr::Exception if an attempt is made to increment
+         *                          past the end of the input.
          */
-        ParserInput operator++(int) {
+        ParserInput operator++(int dummy) {
             if (pos >= len) throw cdr::Exception(L"ParserInput: past EOF");
             return ParserInput(buf, pos++, len);
         }
@@ -62,8 +72,9 @@ namespace cdr {
          * Pre-increments the current parsing position.  Returns a reference
          * to the object which can be used in the expression "*++pInput."
          *
-         *  @throw  cdr::Exception if an attempt is made to increment
-         *                         past the end of the input.
+         *  @return                 reference to the modified object.
+         *  @exception cdr::Exception if an attempt is made to increment
+         *                          past the end of the input.
          */
         ParserInput& operator++() {
             if (pos >= len) throw cdr::Exception(L"ParserInput: past EOF");
@@ -72,15 +83,20 @@ namespace cdr {
         }
 
         /**
-         * Returns the 16-bit character for the current input position.
+         * Accessor method for individual characters in the buffer.
+         *
+         * @return                  16-bit character for the current 
+         *                          input position.
          */
         wchar_t operator *() const { return buf[pos]; }
 
         /**
          * Skips <code>n</code> positions past the current input position.
          *
-         *  @throw  cdr::Exception if an attempt is made to move past
-         *                         the end of the input.
+         *  @param  n               number of positions to skip.
+         *  @return                 reference to the modified object.
+         *  @exception cdr::Exception if an attempt is made to move past
+         *                          the end of the input.
          */
         ParserInput& operator+=(size_t n) { 
             if (pos + n > len)
@@ -98,7 +114,11 @@ namespace cdr {
          * disposing of the allocated memory.  The current position
          * is moved forward past the terminating delimiter.
          *
-         *  @throw  cdr::Exception if matching delimiter is not found
+         *  @param  delim           delimiter marking end of string.
+         *  @return                 address of newly allocated buffer 
+         *                          containing null-terminated copy of
+         *                          extracted string.
+         *  @exception  cdr::Exception if matching delimiter is not found
          *                         before the end of the input buffer.
          */
         const wchar_t* extractString(wchar_t delim);
@@ -110,6 +130,10 @@ namespace cdr {
          * responsible for disposing of the allocated memory.  The current
          * position is moved forward past the terminating delimiter.
          *
+         *  @param  count           number of characters to extract.
+         *  @return                 address of newly allocated buffer 
+         *                          containing null-terminated copy of
+         *                          extracted string.
          *  @throw  cdr::Exception if more characters are requested than
          *                         remain in the input buffer.
          */
@@ -118,6 +142,11 @@ namespace cdr {
         /**
          * Returns <code>true</code> if the input matches the specified
          * null-terminated buffer at the current input position.
+         *
+         *  @param  s               address of null-terminated wide-character
+         *                          string to be matched.
+         *  @return                 <code>true</code> if specified string
+         *                          is matched by the current buffer contents.
          */
         bool matches(const wchar_t* s) const {
             if (!s) return false;
@@ -127,61 +156,125 @@ namespace cdr {
 
         /**
          * Returns the current input position.  Useful for error reporting.
+         *
+         *  @return                 current input position.
          */
         size_t getPos() const { return pos; }
 
         /**
          * Returns the original length of the input string.  Useful for error
          * reporting.
+         *
+         *  @return                 original length of the input string.
          */
         size_t getLen() const { return len; }
 
         /**
          * Reports the token last passed to the parser by the lexical
          * analyzer.  Useful for error reporting.
+         *
+         *  @return                 last token passed to the parser.
          */
         int    getLastTok() const { return lastTok; }
 
         /**
          * Remembers the token last passed to the parser by the lexical
          * analyzer.
+         *
+         *  @param  t               token passed to the parser.
+         *  @return                 token passed by the caller (echoed back).
          */
         int    setLastTok(int t) { return lastTok = t; }
 
         /**
          * Reports the number of characters remaining in the input.  Might be
          * useful for error reporting.
+         *
+         *  @return                 number of characters remaining in the 
+         *                          input buffer.
          */
         size_t getRemainingLen() const { return len - pos; }
 
         /**
          * Returns a read-only pointer to the input buffer.
+         *
+         *  @return                 address of the beginning of the original
+         *                          input buffer.
          */
         const wchar_t* getBuf() const { return buf; }
-
 
         /**
          * Returns a read-only pointer to the current position in the input
          * buffer.  Allows the lexical analyzer to perform look-ahead before
          * it decides what to do with the input.
+         *
+         *  @return                 address of the current position in the
+         *                          input buffer.
          */
         const wchar_t* getRemainingBuf() const { return buf + pos; }
 
     private:
+
+        /**
+         * Self-destruction object to hold the buffer contents.
+         */
         String          str;
+
+        /**
+         * Current processing position in the buffer.
+         */
         size_t          pos;
+
+        /**
+         * Total length of the input buffer, in characters.
+         */
         size_t          len;
+
+        /**
+         * Address of the character array for the input buffer.
+         */
         const wchar_t*  buf;
+
+        /**
+         * Record of the last token passed to the parser.
+         */
         int             lastTok;
+
+        /**
+         * Collection of strings extracted from the buffer, maintained so that
+         * the addresses returned to callers will be valida as long as (but no
+         * longer than) the <code>ParserInput</code> object is valid.
+         */
         StringList      extractedStrings;
+
+        /**
+         * Default constructor.  Creates empty buffer.
+         */
         ParserInput() : buf(L""), pos(0), len(0) {}
-        ParserInput(const ParserInput&);            // Block this.
-        ParserInput operator=(const ParserInput&);  // And this.
+
+        /**
+         * Unimplemented (blocked) copy constructor.
+         *
+         *  @param  pi          reference to object to be copied.
+         */
+        ParserInput(const ParserInput& pi);
+
+        /**
+         * Unimplemented (blocked) assignment operator.
+         *
+         *  @param  pi          reference to object to be copied.
+         *  @return             reference to modified object.
+         */
+        ParserInput& operator=(const ParserInput& pi);
 
         /**
          * Used by the post-increment operator.  Note that not all of the
          * state information is copied from the original object.  See the
          * notes above in the comment for the post-increment operator.
+         *
+         *  @param  b           address of character array for buffer.
+         *  @param  p           current position in buffer.
+         *  @param  l           length of buffer.
          */
         ParserInput(const wchar_t* b, size_t p, size_t l)
             : buf(b), pos(p), len(l) {}
--- cdr/Server/CdrRegEx.h
+++ cdr/Server/CdrRegEx.h
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrRegEx.h,v 1.1 2000-05-03 15:41:36 bkline Exp $
+ * $Id: CdrRegEx.h,v 1.2 2000-05-04 11:52:35 bkline Exp $
  *
  * CDR wrapper for regular expression processing.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/05/03 15:41:36  bkline
+ * Initial revision
+ *
  */
 
 #ifndef CDR_REGEX_
@@ -25,26 +28,205 @@ namespace cdr {
 
     /** @pkg cdr */
 
+    /**
+     * Wrapper class for John Maddock's regular expression package (in case we
+     * need to replace that package at some future time without modifying code
+     * throughout the rest of the system).  First version exposes only the
+     * functionality needed by the Document Validation module.  Additional
+     * features will be added as needed.  In particular, this first version
+     * does not yet support retrieval of the matched substrings.
+     * <P>
+     * Note that Maddock's implementation uses Win32 calls on the Windows
+     * platform.  This is probably a good thing, providing those calls provide
+     * more efficient handling of Unicode character classification than is
+     * present in other packages I've seen (including Henry Spencer's), which
+     * is I suspect why he's using the platform-specific calls.
+     */
     class RegEx : private JM::wregex {
+
     public:
+
+        /**
+         * Default constructor with optional flag for turning off recognition
+         * of differences in case.  Use one of the <code>set()</code> methods
+         * to plug in a regular expression to be used for matching.
+         *
+         *  @param  ignoreCase      <code>true</code> if comparisons should 
+         *                          disregard differences in case for letters; 
+         *                          default is <code>false</code>.
+         */
         RegEx(bool ignoreCase = false) : caseFlag(ignoreCase) {}
+
+        /**
+         * Creates a new regular expression object based on a null-terminated
+         * wide-character string.
+         *
+         *  @param  pattern         address of string containing regular
+         *                          expression, encoded as UTF-16.
+         *  @param  ignoreCase      <code>true</code> if comparisons should 
+         *                          disregard differences in case for letters; 
+         *                          default is <code>false</code>.
+         */
         RegEx(const wchar_t* pattern, bool ignoreCase = false);
+
+        /**
+         * Creates a new regular expression object based on a null-terminated
+         * wide-character string.
+         *
+         *  @param  pattern         reference to string object containing 
+         *                          regular expression, encoded as UTF-16.
+         *  @param  ignoreCase      <code>true</code> if comparisons should 
+         *                          disregard differences in case for letters; 
+         *                          default is <code>false</code>.
+         */
         RegEx(const String& pattern, bool ignoreCase = false);
+
+        /**
+         * Creates a new regular expression object based on a null-terminated
+         * narrow-character string.  String is expected to be encoded as
+         * UTF-8, and is converted internally to UTF-16 encoding by the
+         * constructor.
+         *
+         *  @param  pattern         address of string containing regular
+         *                          expression, encoded as UTF-8.
+         *  @param  ignoreCase      <code>true</code> if comparisons should 
+         *                          disregard differences in case for letters; 
+         *                          default is <code>false</code>.
+         */
         RegEx(const char* pattern, bool ignoreCase = false);
+
+        /**
+         * Creates a new regular expression object based on a standard
+         * narrow-character string object.  String is expected to be encoded
+         * as UTF-8, and is converted internally to UTF-16 encoding.
+         *
+         *  @param  pattern         reference to standard <code>string</code> 
+         *                          object containing regular expression
+         *                          encoded as UTF-8.
+         *  @param  ignoreCase      <code>true</code> if comparisons should 
+         *                          disregard differences in case for letters; 
+         *                          default is <code>false</code>.
+         */
         RegEx(const std::string& pattern, bool ignoreCase = false);
+
+        /**
+         * Accessor method for object's regular expression string.
+         *
+         *  @return                 new <code>String</code> object containing
+         *                          UTF-16 encoded regular expression pattern.
+         */
         String getPattern() const;
-        bool    match(const wchar_t*);
-        bool    match(const String&);
-        bool    match(const char*);
-        bool    match(const std::string&);
+
+        /**
+         * Reports whether the specified string matches the object's regular
+         * expression pattern.
+         *
+         *  @param  s               address of null-terminated string
+         *                          containing string to be tested, encoded as
+         *                          UTF-16.
+         *  @return                 <code>true</code> if <code>s</code>
+         *                          matches the object's regular expression
+         *                          pattern; otherwise <code>false</code>.
+         */
+        bool    match(const wchar_t* s);
+
+        /**
+         * Reports whether the specified string matches the object's regular
+         * expression pattern.
+         *
+         *  @param  s               reference to <code>String</code> object
+         *                          containing string to be tested, encoded as
+         *                          UTF-16.
+         *  @return                 <code>true</code> if <code>s</code>
+         *                          matches the object's regular expression
+         *                          pattern; otherwise <code>false</code>.
+         */
+        bool    match(const String& s);
+
+        /**
+         * Reports whether the specified string matches the object's regular
+         * expression pattern.
+         *
+         *  @param  s               address of null-terminated string
+         *                          containing string to be tested, encoded as
+         *                          UTF-8.
+         *  @return                 <code>true</code> if <code>s</code>
+         *                          matches the object's regular expression
+         *                          pattern; otherwise <code>false</code>.
+         */
+        bool    match(const char* s);
+
+        /**
+         * Reports whether the specified string matches the object's regular
+         * expression pattern.
+         *
+         *  @param  s               reference to <code>string</code> object
+         *                          containing string to be tested, encoded as
+         *                          UTF-8.
+         *  @return                 <code>true</code> if <code>s</code>
+         *                          matches the object's regular expression
+         *                          pattern; otherwise <code>false</code>.
+         */
+        bool    match(const std::string& s);
+
+        /**
+         * Sets the object's regular expression pattern to a new string.
+         *
+         *  @param  pattern         address of null-terminated string
+         *                          containing new regular expression pattern
+         *                          for the object, encoded as UTF-16.
+         */
         void    set(const wchar_t* pattern);
-        void    set(const String& patterh);
+
+        /**
+         * Sets the object's regular expression pattern to a new string.
+         *
+         *  @param  pattern         reference to <code>String</code> object
+         *                          containing new regular expression pattern
+         *                          for the object, encoded as UTF-16.
+         */
+        void    set(const String& pattern);
+
+        /**
+         * Sets the object's regular expression pattern to a new string.
+         *
+         *  @param  pattern         address of null-terminated string
+         *                          containing new regular expression pattern
+         *                          for the object, encoded as UTF-8.
+         */
         void    set(const char* pattern);
-        void    set(const std::string& patterh);
+
+        /**
+         * Sets the object's regular expression pattern to a new string.
+         *
+         *  @param  pattern         reference to <code>string</code> object
+         *                          containing new regular expression pattern
+         *                          for the object, encoded as UTF-8.
+         */
+        void    set(const std::string& pattern);
+
     private:
+
+        /**
+         * Flag remembering whether case should be disregarded for
+         * pattern matching.
+         */
         bool    caseFlag;
-        RegEx(const RegEx&);            // Block this
-        RegEx operator=(const RegEx&);  // This, too.
+
+        /**
+         * Unimplemented (blocked) copy constructor.
+         *
+         *  @param  re              reference to object to be copied.
+         */
+        RegEx(const RegEx&);
+
+        /**
+         * Unimplemented (blocked) assignment operator.
+         *
+         *  @param  re              reference to object to be copied.
+         *  @return                 reference to modified object.
+         */
+        RegEx operator=(const RegEx&);
     };
 
 }
