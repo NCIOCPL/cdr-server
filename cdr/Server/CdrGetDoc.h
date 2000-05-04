56265fa869308ba2fc14dc6c162c2ed1c2c8c887
Bob Kline <bkline@rksystems.com>
2000-05-04 01:06:06 +0000

    <R128> Added ccdoc comments.

--- cdr/Server/CdrGetDoc.h
+++ cdr/Server/CdrGetDoc.h
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrGetDoc.h,v 1.1 2000-05-03 15:41:09 bkline Exp $
+ * $Id: CdrGetDoc.h,v 1.2 2000-05-04 01:06:06 bkline Exp $
  *
  * Internal support functions for CDR document retrieval.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2000/05/03 15:41:09  bkline
+ * Initial revision
+ *
  */
 
 #ifndef CDR_GET_DOC_
@@ -20,7 +23,18 @@ namespace cdr {
 
     /** @pkg cdr */
 
-    extern cdr::String getDocString(const cdr::String&, cdr::db::Connection&);
+    /**
+     * Pulls the components of a CDR document from the database and constructs
+     * a serializable XML version of it.
+     *
+     *  @param  docId       reference to string containing the document's ID.
+     *  @param  conn        reference to an active connection to the CDR
+     *                      database.
+     *  @return             wide-character String object containing XML
+     *                      for the document.
+     */
+    extern cdr::String getDocString(const cdr::String&      docId, 
+                                    cdr::db::Connection&    conn);
 
 }
 
