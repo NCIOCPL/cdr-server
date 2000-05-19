4af78c2495634e1d0ed6f8ba87edee2dcd17bfb6
Bob Kline <bkline@rksystems.com>
2000-05-19 00:06:28 +0000

    <R169> Removed header line.

--- cdr/Server/tCdrGetDoc.cpp
+++ cdr/Server/tCdrGetDoc.cpp
@@ -8,8 +8,7 @@ main(int ac, char** av)
         int id = ac < 2 ? 1 : atoi(av[1]);
         wchar_t docId[32];
         swprintf(docId, L"CDR%010d", id);
-        cdr::String xml = cdr::getDocString(docId, conn);
-        std::wcout << L"XML FOR DOCUMENT " << docId << L":\n" << xml << L"\n";
+        std::wcout << cdr::getDocString(docId, conn) << L"\n";
     }
     catch (cdr::Exception e) {
         std::wcerr << L"*** BAD NEWS: " << e.what() << L"\n";
