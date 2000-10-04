d933ae460c181082a521645025314496595048ea
Bob Kline <bkline@rksystems.com>
2000-10-04 18:28:26 +0000

    <R226> Fixed comment typo; added support for MaxDocs attribute; implemented command to search for link target candidates.

--- cdr/Server/CdrSearch.cpp
+++ cdr/Server/CdrSearch.cpp
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrSearch.cpp,v 1.2 2000-05-03 15:25:41 bkline Exp $
+ * $Id: CdrSearch.cpp,v 1.3 2000-10-04 18:28:26 bkline Exp $
  *
  * Queries the CDR to create subset list of documents.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2000/05/03 15:25:41  bkline
+ * Fixed database statement creation.
+ *
  * Revision 1.1  2000/04/21 13:52:58  bkline
  * Initial revision
  *
@@ -13,11 +16,12 @@
 #include "CdrSearch.h"
 #include "CdrException.h"
 #include "CdrCommand.h"
+#include "CdrLink.h"
 #include "CdrDbPreparedStatement.h"
 #include "CdrDbResultSet.h"
 #include "CdrParserInput.h"
 
-// Remember the parameters set we can set them after prepareStatement().
+// Remember the parameters so we can set them after prepareStatement().
 struct SearchParam {
     SearchParam(const cdr::String& p) : t(STRING), s(p) {}
     SearchParam(int p) : t(INT), i(p) {}
@@ -36,6 +40,7 @@ cdr::String cdr::search(cdr::Session& session,
 {
     // Extract the query from the command buffer.
     cdr::String queryString;
+    int maxRows = 0;
     cdr::dom::Node child = node.getFirstChild();
     while (child != 0) {
         if (child.getNodeType() == cdr::dom::Node::ELEMENT_NODE) {
@@ -43,6 +48,11 @@ cdr::String cdr::search(cdr::Session& session,
             if (name != L"Query")
                 throw cdr::Exception(L"Unexpected element", name);
             queryString = cdr::dom::getTextContent(child);
+            const cdr::dom::Element& docElement =
+                        static_cast<const cdr::dom::Element&>(child);
+            cdr::String attr = docElement.getAttribute (L"MaxDocs");
+            if (!attr.empty())
+                maxRows = wcstoul(attr.c_str(), 0, 10);
         }
         child = child.getNextSibling();
     }
@@ -59,7 +69,7 @@ cdr::String cdr::search(cdr::Session& session,
         std::wcout << L"LAST TOKEN: " << input.getLastTok() << std::endl;
         throw;
     }
-    cdr::String sql = query.getSql();
+    cdr::String sql = query.getSql(maxRows);
 
     // Submit the query to the DBMS.
     SearchParams params;
@@ -86,9 +96,13 @@ cdr::String cdr::search(cdr::Session& session,
     while (rs.next()) {
         if (rows++ == 0)
             response += L"   <QueryResults>\n";
-        int id = rs.getInt(1);
-        wchar_t tmp[40];
-        swprintf(tmp, L"    <DocId>CDR%010ld</DocId>\n", id);
+        int         id      = rs.getInt(1);
+        cdr::String title   = rs.getString(2);
+        wchar_t tmp[1000];
+        swprintf(tmp, L"    <QueryResult>\n     <DocId>CDR%010ld</DocId>\n"
+                      L"     <DocTitle>%.500s</DocTitle>\n"
+                      L"    </QueryResult>\n", 
+                 id, title.c_str());
         response += tmp;
     }
     if (rows > 0)
@@ -157,11 +171,14 @@ std::string extractParams(const cdr::String& sql, SearchParams& params)
 
 cdr::String cdr::QueryNode::markString(const cdr::String& s) const
 {
-    if (op != CONTAINS)
-        return cdr::String(1, STRING_MARK) + s + cdr::String(1, STRING_MARK);
-    else
+    if (op == CONTAINS)
         return cdr::String(1, STRING_MARK) + L"%" + s + L"%"
             + cdr::String(1, STRING_MARK);
+    else if (op == BEGINS)
+        return cdr::String(1, STRING_MARK) + s + L"%"
+            + cdr::String(1, STRING_MARK);
+    else
+        return cdr::String(1, STRING_MARK) + s + cdr::String(1, STRING_MARK);
 }
 
 cdr::String cdr::QueryNode::markInt(const cdr::String& s) const
@@ -178,6 +195,7 @@ cdr::String cdr::QueryNode::getOpString() const
     case LT:        return L" < ";
     case GTE:       return L" >= ";
     case LTE:       return L" <= ";
+    case BEGINS:
     case CONTAINS:  return L" LIKE ";
     default:
         throw cdr::Exception(L"QueryNode: invalid op code");
@@ -264,9 +282,15 @@ cdr::Query::Query()
     hasDocTypeTest  = false;
 }
 
-cdr::String cdr::Query::getSql()
+cdr::String cdr::Query::getSql(int maxDocs)
 {
-    cdr::String sql = L"SELECT document.id FROM document";
+    cdr::String sql = L"SELECT ";
+    if (maxDocs > 0) {
+        wchar_t tBuf[40];
+        swprintf(tBuf, L"TOP %d ", maxDocs);
+        sql += tBuf;
+    }
+    sql += "document.id, document.title FROM document";
     if (tree) {
         checkTablesJoined(tree);
         cdr::String where = L" WHERE ";
@@ -331,4 +355,99 @@ void cdr::Query::freeNodes(cdr::QueryNode* n)
         freeNodes(n->getRightNode());
         delete n;
     }
+}
+
+cdr::String cdr::searchLinks(cdr::Session& session, 
+                             const cdr::dom::Node& node, 
+                             cdr::db::Connection& conn)
+{
+    // See if the client has requested a cap on the number of rows.
+    int maxRows = 0;
+    const cdr::dom::Element& cmdElement =
+                static_cast<const cdr::dom::Element&>(node);
+    cdr::String maxDocsAttr = cmdElement.getAttribute(L"MaxDocs");
+    if (!maxDocsAttr.empty())
+        maxRows = wcstoul(maxDocsAttr.c_str(), 0, 10);
+
+    // Extract the parameters for the search.
+    cdr::String docType;
+    cdr::String elemName;
+    cdr::String titlePattern;
+    cdr::dom::Node child = node.getFirstChild();
+    while (child != 0) {
+        if (child.getNodeType() == cdr::dom::Node::ELEMENT_NODE) {
+            cdr::String name = child.getNodeName();
+            if (name == L"SourceDocType")
+                docType = cdr::dom::getTextContent(child);
+            else if (name == L"SourceElementType")
+                elemName = cdr::dom::getTextContent(child);
+            else if (name == L"TargetTitlePattern")
+                titlePattern = cdr::dom::getTextContent(child);
+        }
+        child = child.getNextSibling();
+    }
+
+    // Make sure we got everything we need.
+    if (docType.empty())
+        throw cdr::Exception(L"Missing required SourceDocType element");
+    else if (elemName.empty())
+        throw cdr::Exception(L"Missing required SourceElementType element");
+    else if (titlePattern.empty())
+        throw cdr::Exception(L"Missing required TargetTitlePattern element");
+
+    // Find out which link target document types are permissible.
+    std::vector<int> targetDocTypes;
+    cdr::link::findTargetDocTypes(conn, elemName, docType, targetDocTypes);
+    if (targetDocTypes.size() < 1)
+        throw cdr::Exception(L"No links permitted from this element");
+
+    // Construct the query.
+    std::string qry = "SELECT ";
+    if (maxRows > 0) {
+        char buf[40];
+        sprintf(buf, "TOP %d ", maxRows);
+        qry += buf;
+    }
+    qry += "id, title FROM document WHERE title LIKE ?";
+    if (targetDocTypes.size() == 1)
+        qry += " AND doc_type = ?";
+    else {
+        qry += " AND doc_type IN (";
+        const char* placeHolder = "?";
+        for (size_t i = 0; i < targetDocTypes.size(); ++i) {
+            qry += placeHolder;
+            placeHolder = ",?";
+        }
+        qry += ")";
+    }
+    qry += " ORDER BY id";
+
+    // Submit the query to the DBMS.
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    int pos = 1;
+    stmt.setString(pos++, titlePattern);
+    for (size_t i = 0; i < targetDocTypes.size(); ++i)
+        stmt.setInt(pos++, targetDocTypes[i]);
+    cdr::db::ResultSet rs = stmt.executeQuery();
+
+    // Construct the response.
+    cdr::String response = L"<CdrSearchLinksResp>";
+    int rows = 0;
+    while (rs.next()) {
+        if (rows++ == 0)
+            response += L"<QueryResults>";
+        int         id      = rs.getInt(1);
+        cdr::String title   = rs.getString(2);
+        wchar_t tmp[1000];
+        swprintf(tmp, L"<QueryResult><DocId>CDR%010ld</DocId>"
+                      L"<DocTitle>%.500s</DocTitle>"
+                      L"</QueryResult>", 
+                 id, title.c_str());
+        response += tmp;
+    }
+    if (rows > 0)
+        response += L"</QueryResults></CdrSearchLinksResp>";
+    else
+        response += L"<QueryResults/></CdrSearchLinksResp>";
+    return response;
 }
