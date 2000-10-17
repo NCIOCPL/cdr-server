31e0447f2b22331dde0ca0202160e5992b513ccd
ameyer <webcommauto@users.noreply.github.com>
2000-10-17 21:22:10 +0000

    <R246> Added overloaded getDocString for versioned doc.

--- cdr/Server/CdrGetDoc.h
+++ cdr/Server/CdrGetDoc.h
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrGetDoc.h,v 1.3 2000-09-25 14:04:09 mruben Exp $
+ * $Id: CdrGetDoc.h,v 1.4 2000-10-17 21:22:10 ameyer Exp $
  *
  * Internal support functions for CDR document retrieval.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2000/09/25 14:04:09  mruben
+ * added getDocCtlString to get information from document table
+ *
  * Revision 1.2  2000/05/04 01:06:06  bkline
  * Added ccdoc comments.
  *
@@ -36,15 +39,33 @@ namespace cdr {
      *  @return             wide-character String object containing XML
      *                      for the document.
      */
-    extern cdr::String getDocString(const cdr::String&      docId, 
+    extern cdr::String getDocString(const cdr::String&      docId,
                                     cdr::db::Connection&    conn);
 
+    /**
+     * Overloaded version of getDocString which pulls a previous version
+     * of the document from the version control system instead of the
+     * document table.
+     *
+     * Not all control elements will be the same.
+     *
+     *  @param  docId       reference to string containing the document's ID.
+     *  @param  conn        reference to an active connection to the CDR
+     *                      database.
+     *  @param  versionNum  version number to retrieve.
+     *  @return             wide-character String object containing XML
+     *                      for the document.
+     */
+    extern cdr::String getDocString(const cdr::String&      docId,
+                                    cdr::db::Connection&    conn,
+                                    int versionNum);
+
     /**@#-*/
-  
+
     namespace DocCtlComponents
     {
     /**@#+*/
-      
+
       /**
        * The components of CdrCtl
        */
@@ -53,9 +74,9 @@ namespace cdr {
              DocValDate =   0x0002,
              DocTitle =     0x0004,
              DocComment =   0x0008
-      };             
+      };
     }
-  
+
     /**
      * Pulls the control components of a CDR document from the database
      * and constructs a serializable XML version of it.
@@ -67,7 +88,7 @@ namespace cdr {
      *  @return             wide-character String object containing XML
      *                      for the document.
      */
-    extern cdr::String getDocCtlString(const cdr::String&      docId, 
+    extern cdr::String getDocCtlString(const cdr::String&      docId,
                                        cdr::db::Connection&    conn,
                                        int elements
                                            = cdr::DocCtlComponents::all);
