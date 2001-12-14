ec0921fdf43a535162710ed5b2d27a6eb46fdb40
Bob Kline <bkline@rksystems.com>
2001-12-14 18:28:06 +0000

    <R916> Added heapDebugging flag.

--- cdr/Server/HeapDebug.cpp
+++ cdr/Server/HeapDebug.cpp
@@ -1,9 +1,12 @@
 /*
- * $Id: HeapDebug.cpp,v 1.1 2001-12-14 15:22:20 bkline Exp $
+ * $Id: HeapDebug.cpp,v 1.2 2001-12-14 18:28:06 bkline Exp $
  *
  * Instrumentation for tracking down dynamic memory leaks.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2001/12/14 15:22:20  bkline
+ * Initial revision
+ *
  */
 
 #include "HeapDebug.h"
@@ -14,29 +17,45 @@
 #include <iostream>
 #include <malloc.h>
 
+static bool heapDebugging = false;
+void setHeapDebugging(bool flag) { heapDebugging = flag; }
+
 static _CrtMemState memState1, memState2, memState3;
-void memStart() { _CrtMemCheckpoint(&memState1); }
+
+void memStart() {
+    if (heapDebugging) { 
+        _CrtMemCheckpoint(&memState1); 
+    } 
+}
+
 void memReport() {
-    static bool first = true;
-    if (first) {
-        _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-        _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
-        first = false;
+    if (heapDebugging) {
+        static bool first = true;
+        if (first) {
+            _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+            _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+            first = false;
+        }
+        _CrtMemCheckpoint(&memState2);
+        _CrtMemDifference(&memState3, &memState1, &memState2);
+        _CrtMemDumpStatistics(&memState3);
+        _CrtMemDumpAllObjectsSince(&memState1);
     }
-    _CrtMemCheckpoint(&memState2);
-    _CrtMemDifference(&memState3, &memState1, &memState2);
-    _CrtMemDumpStatistics(&memState3);
-    _CrtMemDumpAllObjectsSince(&memState1);
 }
 
 long heapUsed() 
 {
-    _CrtMemState cms;
-    _CrtMemCheckpoint(&cms);
-    return (long)cms.lSizes[_NORMAL_BLOCK];
+    if (heapDebugging) {
+        _CrtMemState cms;
+        _CrtMemCheckpoint(&cms);
+        return (long)cms.lSizes[_NORMAL_BLOCK];
+    }
+    return 0;
 }
 
 void showHeapUsed(const char *where)
 {
-    std::cerr << where << ": " << heapUsed() << std::endl;
+    if (heapDebugging) {
+        std::cerr << where << ": " << heapUsed() << std::endl;
+    }
 }
