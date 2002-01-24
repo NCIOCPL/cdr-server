6a3d264f130212cfe4074a3dfe4f2fae9d4eba05
ameyer <webcommauto@users.noreply.github.com>
2002-01-24 23:12:38 +0000

    <R1121> Set environment variables for a local build on a network machine, e.g,   programmer's workstation. Must be modified and renamed to be useful.

---
+++ cdr/Server/setcdrmak.bat
@@ -0,0 +1,30 @@
+:: setcdrmake.bat
+::
+:: Sets up variables for compiling CDR on my local machine.
+
+:: IMPORTANT: After extracting this file from CVS, rename it
+::  before modifying it.  Otherwise you risk having CVS put your 
+::  changes back to the repository and/or have someone else's changes
+::  (who didn't read these instructions) clobber yours.
+
+:: Uncomment and modify any of these if not using these include
+::  files and libraries on mmdb2 (or wherever the CDR resides).
+rem set CDR=n:/cdr
+rem set XML4C=n:/usr/xml4c
+rem set SABLOT=n:/usr/sablot
+rem set REGEX=n:/usr/regex
+rem set BISON_SIMPLE=n:/etc/bison.cdrtest
+
+:: If any of the above variables are set, then you must either uncomment
+::   the following line to tell make/nmake to override makefile
+::   variables with environment variables (be careful about the
+::   rest of your environment!), or use
+::      make -e
+rem set MAKEFLAGS=e
+
+:: DRV sets a drive path before all of the above variables.
+:: If finding CDR, XML4C, SABLOT, REGEX, BISON, BISON_SIMPLE on mmdb2, d:
+::   just set DRV=<your__mapped_drive_leter:>.
+:: DRV is NOT set in the makefile, so this is not an override and
+::   does not require make -e or MAKEFLAGS.
+set DRV=n:
