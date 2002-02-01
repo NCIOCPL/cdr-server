b4ee97fae75eeac46d8fe9c1e7f877f9cc32e6e1
Bob Kline <bkline@rksystems.com>
2002-02-01 20:49:19 +0000

    <R1208> Modified query to use expired column.

--- cdr/Server/CdrGetGrp.cpp
+++ cdr/Server/CdrGetGrp.cpp
@@ -1,10 +1,13 @@
 
 /*
- * $Id: CdrGetGrp.cpp,v 1.3 2001-04-13 12:25:13 bkline Exp $
+ * $Id: CdrGetGrp.cpp,v 1.4 2002-02-01 20:49:19 bkline Exp $
  *
  * Retrieves information about requested group.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2001/04/13 12:25:13  bkline
+ * Retrieve grp.name so case of group name is preserved.
+ *
  * Revision 1.2  2000/05/03 15:25:41  bkline
  * Fixed database statement creation.
  *
@@ -82,7 +85,9 @@ cdr::String cdr::getGrp(cdr::Session& session,
             "  FROM usr u,"
             "       grp_usr g"
             " WHERE g.grp = ?"
-            "   AND g.usr = u.id";
+            "   AND g.usr = u.id"
+            "   AND (u.expired IS NULL"
+            "    OR u.expired > GETDATE())";
     cdr::db::PreparedStatement usrSelect = conn.prepareStatement(query);
     usrSelect.setInt(1, gid);
     cdr::db::ResultSet usrRs = usrSelect.executeQuery();
