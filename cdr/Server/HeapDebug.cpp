30c7a5cc4ed8d4930bfc6ea7ea8a2a11855a32f4
Bob Kline <bkline@rksystems.com>
2002-03-06 21:58:32 +0000

    <R1459> Tracking all heap block types, not just normal blocks.

--- cdr/Server/HeapDebug.cpp
+++ cdr/Server/HeapDebug.cpp
@@ -1,9 +1,12 @@
 /*
- * $Id: HeapDebug.cpp,v 1.4 2002-03-04 21:22:38 bkline Exp $
+ * $Id: HeapDebug.cpp,v 1.5 2002-03-06 21:58:32 bkline Exp $
  *
  * Instrumentation for tracking down dynamic memory leaks.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2002/03/04 21:22:38  bkline
+ * Added code to report on more types of heap memory.
+ *
  * Revision 1.3  2002/03/04 20:52:59  bkline
  * Modified headers to conform with MS advice on heap debugging.
  *
@@ -33,6 +36,11 @@ static _CrtMemState memState1, memState2, memState3;
 
 void memStart() {
     if (heapDebugging) { 
+        static bool first = true;
+        if (first) {
+            _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_CHECK_CRT_DF);
+            first = false;
+        }
         _CrtMemCheckpoint(&memState1); 
     } 
 }
@@ -67,6 +75,17 @@ long heapUsed()
 void showHeapUsed(const char *where)
 {
     if (heapDebugging) {
-        std::cerr << where << ": " << heapUsed() << std::endl;
+        //std::cerr << where << ": " << heapUsed() << std::endl;
+        _CrtMemState cms;
+        _CrtMemCheckpoint(&cms);
+        std::cerr << where << ": " << (long)cms.lSizes[_NORMAL_BLOCK]
+                           <<  "+" << (long)cms.lSizes[_CRT_BLOCK]
+                           <<  "+" << (long)cms.lSizes[_IGNORE_BLOCK]
+                           <<  "+" << (long)cms.lSizes[_CLIENT_BLOCK]
+                           <<  "=" << (long)cms.lSizes[_NORMAL_BLOCK] +
+                                      (long)cms.lSizes[_CRT_BLOCK]    +
+                                      (long)cms.lSizes[_IGNORE_BLOCK] +
+                                      (long)cms.lSizes[_CLIENT_BLOCK]
+                                   << std::endl;
     }
 }
