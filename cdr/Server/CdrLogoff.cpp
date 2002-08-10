709a44cdf23daef9ceb002d8a2f4de7a2da38cf4
Bob Kline <bkline@rksystems.com>
2002-08-10 19:28:21 +0000

    <R2282> Added test to avoid logging off special 'guest' session.

--- cdr/Server/CdrLogoff.cpp
+++ cdr/Server/CdrLogoff.cpp
@@ -1,9 +1,13 @@
 /*
- * $Id: CdrLogoff.cpp,v 1.4 2000-05-09 21:08:19 bkline Exp $
+ * $Id: CdrLogoff.cpp,v 1.5 2002-08-10 19:28:21 bkline Exp $
  *
  * Closes a CDR session.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2000/05/09 21:08:19  bkline
+ * Replaced attempt to use private Session field directly with accessor
+ * method call.
+ *
  * Revision 1.3  2000/05/09 20:14:56  bkline
  * Replaced direct manipulation of Session fields with clear() method.
  *
@@ -23,11 +27,16 @@ cdr::String cdr::logoff(cdr::Session& session,
                         const cdr::dom::Node&,
                         cdr::db::Connection& conn)
 {
-    // Pop the logoff date/time into the session table.
-    std::string query = "UPDATE session SET ended = GETDATE() WHERE id = ?";
-    cdr::db::PreparedStatement update = conn.prepareStatement(query);
-    update.setInt(1, session.getId());
-    update.executeQuery();
+    // Don't log the special guest account off.
+    if (session.getName() != L"guest") {
+
+        // Pop the logoff date/time into the session table.
+        std::string query = 
+            "UPDATE session SET ended = GETDATE() WHERE id = ?";
+        cdr::db::PreparedStatement update = conn.prepareStatement(query);
+        update.setInt(1, session.getId());
+        update.executeQuery();
+    }
 
     
     // Clear out the session object's state.
