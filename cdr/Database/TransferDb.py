e87cd7bfd863da67c8be5e49a27437fb47b3b6c5
pzhang <webcommauto@users.noreply.github.com>
2002-08-22 17:57:38 +0000

    <R2350> Transfer cdr from BACH to MOHLER.

--- cdr/Database/TransferDb.py
+++ cdr/Database/TransferDb.py
@@ -1,10 +1,13 @@
 #----------------------------------------------------------------------
 #
-# $Id: TransferDb.py,v 1.2 2002-02-14 19:03:28 bkline Exp $
+# $Id: TransferDb.py,v 1.3 2002-08-22 17:57:38 pzhang Exp $
 #
-# Transfers newly generated cdr database from CDRDEV to MMDB2.
+# Transfers newly generated cdr database from BACH to MOHLER.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2002/02/14 19:03:28  bkline
+# Switched from test database (foo) to cdr.
+#
 # Revision 1.1  2001/12/24 01:07:50  bkline
 # Initial revision
 #
@@ -13,7 +16,7 @@
 import cdrdb
 
 db   = 'cdr'
-conn = cdrdb.connect(dataSource = 'CDRDEV')
+conn = cdrdb.connect(dataSource = 'BACH')
 curs = conn.cursor()
 conn.setAutoCommit()
 curs.execute("USE master")
@@ -21,14 +24,14 @@ curs.execute("EXEC sp_dboption '%s', 'single user', 'TRUE'" % db)
 curs.execute("BACKUP DATABASE %s TO DISK = 'd:/cdr/Database/cdr.bak'" % db)
 curs.execute("EXEC sp_dboption '%s', 'single user', 'FALSE'" % db)
 
-conn = cdrdb.connect(dataSource = 'MMDB2')
+conn = cdrdb.connect(dataSource = 'MOHLER')
 curs = conn.cursor()
 conn.setAutoCommit()
 curs.execute("USE master")
 curs.execute("EXEC sp_dboption '%s', 'single user', 'TRUE'" % db)
 curs.execute("""\
 RESTORE DATABASE %s 
-FROM DISK = '\\\\CDRDEV\\cdr\\Database\\cdr.bak'""" % db)
+FROM DISK = '\\\\BACH\\cdr\\Database\\cdr.bak'""" % db)
 curs.execute("EXEC sp_dboption '%s', 'single user', 'FALSE'" % db)
 curs.execute("USE %s" % db)
 curs.execute("EXEC sp_changedbowner 'cdr'")
