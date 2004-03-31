788a292689e90676e827b816a40b075872832d6e
ameyer <webcommauto@users.noreply.github.com>
2004-03-31 03:11:08 +0000

    <R4949> Added prototype for analyzeException().

--- cdr/Server/catchexp.h
+++ cdr/Server/catchexp.h
@@ -1,7 +1,7 @@
 /************************************************************
 * catchexp.h                                                *
 *                                                           *
-*   Header file for catchexp.c, subroutine for              *
+*   Header file for subroutines for                         *
 *   intercepting crashes and preventing Windows             *
 *   from writing a message to the console.                  *
 *                                                           *
@@ -9,12 +9,22 @@
 
 #ifndef CATCHEXP_H
 #define CATCHEXP_H
+#include <windows.h>
+#include <process.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void set_exception_catcher (char *logfilename);
+/* Replaces Windows default exception catcher with our own
+ * This can still be used, but is superseded by one that
+ *  throws a C++ exception.
+ */
+
+void set_exception_catcher (char *logfilename, int abortOnError);
+
+/* Interpret info in a Windows structured exception block */
+char *analyzeException (struct _EXCEPTION_POINTERS *excp);
 
 #ifdef __cplusplus
 }
