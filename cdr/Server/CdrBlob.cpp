b4f14bd211c952ea40e7116384d09854faa1251c
ameyer <webcommauto@users.noreply.github.com>
2004-11-05 05:18:08 +0000

    <R5619> Added many new blob management functions for new blob concepts.

--- cdr/Server/CdrBlob.cpp
+++ cdr/Server/CdrBlob.cpp
@@ -1,20 +1,26 @@
 /*
- * $Id: CdrBlob.cpp,v 1.1 2004-10-22 03:20:42 ameyer Exp $
+ * $Id: CdrBlob.cpp,v 1.2 2004-11-05 05:18:08 ameyer Exp $
  *
  * Manage the occurrence of blobs and references to blobs in the
  * CDR database.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2004/10/22 03:20:42  ameyer
+ * New document blob management subroutines.
+ * First version not yet tested but installed in CVS in case someone
+ * wants to see how it is intended to work.
+ *
  */
 
+#include <iostream> // Debug
 #include <string>
 #include "CdrBlob.h"
+#include "CdrBlobExtern.h"
 #include "CdrDbConnection.h"
 #include "CdrDbStatement.h"
 #include "CdrDbResultSet.h"
 #include "CdrDbPreparedStatement.h"
 #include "CdrException.h"
-
 /**
  * Put an entry in the doc_blob_usage table
  */
@@ -23,9 +29,8 @@ static void addDocBlobUsage(
     int docId,
     int blobId
 ) {
-    std::string qry =
-        "INSERT INTO doc_blob_usage (doc_id, blob_id) VALUES (?,?)";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+        "INSERT INTO doc_blob_usage (doc_id, blob_id) VALUES (?,?)");
     stmt.setInt(1, docId);
     stmt.setInt(2, blobId);
     stmt.executeUpdate();
@@ -40,8 +45,8 @@ static void repDocBlobUsage(
     int docId,
     int blobId
 ) {
-    std::string qry = "UPDATE doc_blob_usage SET blob_id=? WHERE doc_id=?";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+        "UPDATE doc_blob_usage SET blob_id=? WHERE doc_id=?");
     stmt.setInt(1, blobId);
     stmt.setInt(2, docId);
     stmt.executeUpdate();
@@ -49,39 +54,56 @@ static void repDocBlobUsage(
 }
 
 /**
- * Delete an existing entry in the doc_blob_usage table
+ * Delete an existing entry in the doc_blob_usage table.
+ * No checking done.
+ *
+ *  @param conn             Database connection.
+ *  @param docId            ID of the doc associated with this blob.
  */
 static void delDocBlobUsage(
     cdr::db::Connection& conn,
     int docId
 ) {
-    std::string qry = "DELETE FROM doc_blob_usage WHERE doc_id=?";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+        "DELETE FROM doc_blob_usage WHERE doc_id=?");
     stmt.setInt(1, docId);
     stmt.executeUpdate();
     stmt.close();
 }
 
 /**
- * Put an entry in the version_blob_usage table.
- * Replace and delete are not required because old versions
- *   never change.
+ * Put an entry in the version_blob_usage table for the last version
+ * of the document.
+ *
  */
-static void addVersionBlobUsage(
+void cdr::addVersionBlobUsage(
     cdr::db::Connection& conn,
     int docId,
-    int version,
     int blobId
 ) {
-    std::string qry =
-        "INSERT INTO version_blob_usage (doc_id, version, blob_id) "
-        " VALUES (?,?,?)";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
-    stmt.setInt(1, docId);
-    stmt.setInt(2, version);
-    stmt.setInt(3, blobId);
-    stmt.executeUpdate();
-    stmt.close();
+    // Get the number of the last version
+    cdr::db::PreparedStatement verStmt = conn.prepareStatement(
+        "SELECT MAX(num) FROM doc_version WHERE id=?");
+    verStmt.setInt(1, docId);
+    cdr::db::ResultSet rs = verStmt.executeQuery();
+    if (!rs.next()) {
+        wchar_t msg[160];
+        swprintf(msg, L"Attempt to add blob=%d to doc version for doc=%d"
+                      L" but that doc has no version.", blobId, docId);
+        throw cdr::Exception(msg);
+    }
+    int ver = rs.getInt(1);
+    verStmt.close();
+
+    // Insert a row into the version_blob_usage table
+    cdr::db::PreparedStatement insStmt = conn.prepareStatement(
+        "INSERT INTO version_blob_usage (doc_id, blob_id, doc_version) "
+        " VALUES (?, ?, ?)");
+    insStmt.setInt(1, docId);
+    insStmt.setInt(2, blobId);
+    insStmt.setInt(3, ver);
+    insStmt.executeUpdate();
+    insStmt.close();
 }
 
 /**
@@ -93,7 +115,7 @@ static void addVersionBlobUsage(
  *  @return             Blob id.
  *                      0 if no blob associated with id(s).
  */
-int lookupBlobId(
+static int lookupBlobId(
     cdr::db::Connection& conn,
     int docId,
     int version
@@ -104,7 +126,7 @@ int lookupBlobId(
     if (version)
         // Look for doc_version blob
         qry = "SELECT blob_id FROM version_blob_usage "
-              " WHERE doc_id=? AND version=?";
+              " WHERE doc_id=? AND doc_version=?";
     else
         // Look for doc blob
         qry = "SELECT blob_id FROM doc_blob_usage WHERE doc_id=?";
@@ -129,15 +151,14 @@ int lookupBlobId(
  *  @return             True  = Blob has been versioned at least once.
  *                      False = No versions of this blob.
  */
-bool isBlobVersioned(
+static bool isBlobVersioned(
     cdr::db::Connection& conn,
     int blobId
 ) {
     bool isVersioned = false;
 
-    std::string qry =
-        "SELECT TOP 1 doc_id FROM version_blob_usage WHERE blob_id=?";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+        "SELECT TOP 1 doc_id FROM version_blob_usage WHERE blob_id=?");
     stmt.setInt(1, blobId);
     cdr::db::ResultSet rs = stmt.executeQuery();
     if (rs.next())
@@ -155,18 +176,16 @@ bool isBlobVersioned(
  *  @param conn         Database connection.
  *  @param blob         Reference to the blob itself.
  *  @param docId        Find blob associated with this document.
- *  @param version      If non-zero, look for version, not doc association.
  *  @return             Blob id for new blob.
  */
 static int addBlob(
     cdr::db::Connection& conn,
-    cdr::Blob blob,
-    int docId,
-    int version
+    cdr::Blob& blob,
+    int docId
 ) {
     // First insert the new blob, getting the new blob id
-    std::string qry = "INSERT INTO doc_blob (data) VALUES (?)";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+        "INSERT INTO doc_blob (data) VALUES (?)");
     stmt.setBytes(1, blob);
     stmt.executeUpdate();
     int blobId = conn.getLastIdent();
@@ -177,9 +196,9 @@ static int addBlob(
     if (lookupBlobId(conn, docId, 0))
         repDocBlobUsage(conn, docId, blobId);
 
-    // If we're creating a version, make the new version association
-    if (version)
-        addVersionBlobUsage(conn, docId, version, blobId);
+    // Else establish the asociation
+    else
+        addDocBlobUsage(conn, docId, blobId);
 
     return blobId;
 }
@@ -193,15 +212,13 @@ static int addBlob(
  *  @param conn         Database connection.
  *  @param blob         Reference to the blob itself.
  *  @param docId        Replace blob associated with this document.
- *  @param version      If non-zero, also create a version usage for this blob.
  *  @param blobId       ID of the blob to replace.
  *  @return             Void.
  */
 static void replaceBlob(
     cdr::db::Connection& conn,
-    cdr::Blob blob,
+    cdr::Blob& blob,
     int docId,
-    int version,
     int blobId
 ) {
     // Validate
@@ -213,17 +230,12 @@ static void replaceBlob(
     }
 
     // First replace the blob itself
-    std::string qry = "UPDATE doc_blob set data=? WHERE blob_id=?";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+        "UPDATE doc_blob set data=? WHERE id=?");
     stmt.setBytes(1, blob);
     stmt.setInt(2, blobId);
     stmt.executeUpdate();
     stmt.close();
-
-    // If we're creating a version, make the new version association
-    // Document association already exists
-    if (version)
-        addVersionBlobUsage(conn, docId, version, blobId);
 }
 
 /**
@@ -232,41 +244,145 @@ static void replaceBlob(
  * Assumptions:
  *   Blob has never been versioned.
  *   User wishes to eliminate the blob from the current working document.
+ *   Caller must also call delDocBlobUsage().
  *
  *  @param conn         Database connection.
- *  @param docId        ID of the doc associated with blob.
+ *  @param blobId       ID of the blob to delete.
+ *  @param docId        Passed solely for use in error messages.
  *  @return             Void.
  */
 static void delBlob(
     cdr::db::Connection& conn,
+    int blobId,
     int docId
 ) {
     wchar_t msg[80];    // For error messages
 
-    // Find the blob id for the document
-    int blobId = lookupBlobId(conn, docId, 0);
-
     // Validate
     if (!blobId) {
-        swprintf(msg, L"Attempt to delete non-existent blob for doc=%d",
-                 docId);
+        swprintf(msg,
+                 L"Attempt to delete non-existent blob docId=%d, blobId=%d",
+                 docId, blobId);
         throw cdr::Exception(msg);
     }
 
     // Validate no versioning exists
     if (isBlobVersioned(conn, blobId)) {
-        swprintf(msg, L"Attempt to delete versioned blob for doc=%d",
-                 docId);
+        swprintf(msg,
+                 L"Attempt to delete versioned blob docId=%d, blobId=%d",
+                 docId, blobId);
         throw cdr::Exception(msg);
     }
 
-    // Break the association between document and blob, there can only be one
-    delDocBlobUsage(conn, docId);
-
     // Now delete the blob
-    std::string qry = "DELETE FROM doc_blob WHERE blob_id=?";
-    cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+        "DELETE FROM doc_blob WHERE id=?");
     stmt.setInt(1, blobId);
     stmt.executeUpdate();
     stmt.close();
+}
+
+/* Get a blob from the database.
+ * See CdrBlobExtern.h for documentation.
+ */
+cdr::Blob *cdr::getBlobPtrByBlobId(
+    cdr::db::Connection& conn,
+    int blobId
+) {
+    cdr::Blob *pBlob = 0;
+
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+            "SELECT data FROM doc_blob WHERE id = ?");
+    stmt.setInt(1, blobId);
+    cdr::db::ResultSet rs = stmt.executeQuery();
+    if (rs.next())
+        pBlob = new cdr::Blob(rs.getBytes(1));
+    stmt.close();
+
+    return pBlob;
+}
+
+/* Get a blob from the database by doc id.
+ * See CdrBlobExtern.h for documentation.
+ */
+cdr::Blob *cdr::getBlobPtrByDocId(
+    cdr::db::Connection& conn,
+    int docId
+) {
+    cdr::Blob *pBlob = 0;
+
+    cdr::db::PreparedStatement stmt = conn.prepareStatement(
+            "SELECT data FROM doc_blob JOIN doc_blob_usage "
+            "    ON doc_blob.id = doc_blob_usage.blob_id "
+            " WHERE doc_blob_usage.doc_id = ?");
+    stmt.setInt(1, docId);
+    cdr::db::ResultSet rs = stmt.executeQuery();
+    if (rs.next())
+        pBlob = new cdr::Blob(rs.getBytes(1));
+    stmt.close();
+
+    return pBlob;
+}
+
+/* Get a blob from the database by doc id + version.
+ * See CdrBlobExtern.h for documentation.
+ */
+cdr::Blob *cdr::getBlobPtrByDocVersion(
+    cdr::db::Connection& conn,
+    int docId,
+    int version
+) {
+    // Simple brute force - calling this should be pretty rare
+    int blobId = lookupBlobId(conn, docId, version);
+    if (blobId)
+        return cdr::getBlobPtrByBlobId(conn, blobId);
+    return 0;
+}
+
+/* Add or replace a blob in the database.
+ * See CdrBlobExtern.h for interface.
+ */
+int cdr::setBlob(
+    cdr::db::Connection& conn,
+    cdr::Blob& blob,
+    int docId
+) {
+    // Find id by inspecting the database, gets 0 if not found
+    int blobId = lookupBlobId(conn, docId, 0);
+
+    // Setting a zero length blob means delete it from the current working doc
+    if (blob.length() == 0) {
+        // Break the association between blob and doc
+        delDocBlobUsage(conn, docId);
+
+        if (!isBlobVersioned(conn, blobId))
+            // Nothing else references this blob, delete the blob itself
+            delBlob(conn, blobId, docId);
+
+        // Nothing more to do.  No more blobId.
+        return 0;
+    }
+
+    // If this doc already has a blob, is it different from passed blob?
+    bool blobIsNew = true;
+    if (blobId) {
+        cdr::Blob *pBlob = cdr::getBlobPtrByBlobId(conn, blobId);
+        if (pBlob) {
+            blobIsNew = pBlob->compare(blob) ? true : false;
+            delete pBlob;
+        }
+    }
+
+    if (blobIsNew) {
+        if (blobId && !isBlobVersioned(conn, blobId))
+            // Blob is different, but it was never versioned
+            // We replace the blob that was there.  Old one disappears.
+            replaceBlob(conn, blob, docId, blobId);
+        else
+            // Even if this doc had a blob, it now gets a new one
+            // But we can't overwrite the old one because it's versioned
+            blobId = addBlob(conn, blob, docId);
+    }
+
+    return blobId;
 }
