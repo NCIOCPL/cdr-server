d1a7f8bc95614b9ad37ca59f15000ba0c9c38086
Bob Kline <bkline@rksystems.com>
2005-03-08 14:43:35 +0000

    <R5902> Added jobs for emailer lookup jobs.

--- cdr/Database/InstallEmailerJobs.sql
+++ cdr/Database/InstallEmailerJobs.sql
@@ -1,9 +1,12 @@
 /*
- * $Id: InstallEmailerJobs.sql,v 1.2 2004-11-23 16:47:01 bkline Exp $
+ * $Id: InstallEmailerJobs.sql,v 1.3 2005-03-08 14:43:35 bkline Exp $
  *
  * Script to install job to update emailer tracking information.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/11/23 16:47:01  bkline
+ * Fixed mis-named step.
+ *
  * Revision 1.1  2004/06/19 12:27:08  bkline
  * Scheduled update of electronic mailer tracking documents.
  *
@@ -27,5 +30,21 @@ EXEC sp_add_jobschedule @job_name          = 'Emailer Tracking Update',
                         @freq_interval     = 1,
                         @freq_subday_type  = 4, -- minutes
                         @freq_subday_interval = 1,
+                        @enabled           = 1
+GO
+EXEC sp_add_job         @job_name          = 'Emailer Lookup Tables',
+                        @category_name     = 'CDR Jobs'
+GO
+EXEC sp_add_jobstep     @job_name          = 'Emailer Lookup Tables',
+                        @step_name         = 'Upload New Tables',
+                        @subsystem         = 'CMDEXEC',
+                        @command      = 'd:\cdr\bin\GetEmailerLookupTables.cmd'
+GO
+EXEC sp_add_jobschedule @job_name          = 'Emailer Lookup Tables',
+                        @name              = 'Lookup Refresh Schedule',
+                        @freq_type         = 4, -- daily
+                        @freq_interval     = 1,
+                        @freq_subday_type  = 4, -- minutes
+                        @freq_subday_interval = 2,
                         @enabled           = 1
 GO
