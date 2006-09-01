bc4c32295323ec3b36eab7bcc89b6c7364f997a4
ameyer <webcommauto@users.noreply.github.com>
2006-09-01 02:07:10 +0000

    <R6841> Minor safety check on mappability.

--- cdr/Server/CdrExternalMap.cpp
+++ cdr/Server/CdrExternalMap.cpp
@@ -1,9 +1,12 @@
 /*
- * $Id: CdrExternalMap.cpp,v 1.1 2004-08-11 17:48:15 bkline Exp $
+ * $Id: CdrExternalMap.cpp,v 1.2 2006-09-01 02:07:10 ameyer Exp $
  *
  * Commands for manipulating mappings of external values to CDR documents.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2004/08/11 17:48:15  bkline
+ * Adding new command CdrAddExternalMapping.
+ *
  */
 
 #include "CdrCommand.h"
@@ -29,6 +32,8 @@ cdr::String cdr::addExternalMapping(cdr::Session& session,
     String usage(true);
     String value(true);
     String cdrId(true);
+    String bogus(L"N");
+    String mappable("Y");
     dom::Node child = commandNode.getFirstChild();
     while (child != 0) {
         String name = child.getNodeName();
@@ -38,18 +43,26 @@ cdr::String cdr::addExternalMapping(cdr::Session& session,
             value = dom::getTextContent(child);
         else if (name == L"CdrId")
             cdrId = dom::getTextContent(child);
+        else if (name == L"Bogus")
+            bogus = dom::getTextContent(child).toUpperCase();
+        else if (name == L"Mappable")
+            mappable = dom::getTextContent(child).toUpperCase();
         child = child.getNextSibling();
     }
+
+    // Validate
     if (usage.size() == 0)
         throw Exception(L"Missing usage name");
     if (value.isNull())
         throw Exception(L"Missing mapping value");
+    ynCheck(bogus, true, L"Bogus element");
+    ynCheck(mappable, true, L"Mappable element");
 
     // Find the usage ID and action name.
     std::string usageQuery = "SELECT u.id, a.name         "
                              "  FROM external_map_usage u "
                              "  JOIN action a             "
-                             "    ON a.id = u.auth_action " 
+                             "    ON a.id = u.auth_action "
                              " WHERE u.name = ?           ";
     db::PreparedStatement usageStatement = conn.prepareStatement(usageQuery);
     usageStatement.setString(1, usage);
@@ -65,8 +78,9 @@ cdr::String cdr::addExternalMapping(cdr::Session& session,
 
     // Add the row.
     std::string insertQuery =
-        "INSERT INTO external_map (usage, value, doc_id, usr, last_mod) "
-        "     VALUES (?, ?, ?, ?, GETDATE())                            ";
+        "INSERT INTO external_map "
+        "     (usage, value, doc_id, usr, last_mod, bogus, mappable) "
+        "     VALUES (?, ?, ?, ?, GETDATE(), ?, ?)                            ";
     db::PreparedStatement insertStatement = conn.prepareStatement(insertQuery);
     Int docId(true);
     if (!cdrId.isNull())
@@ -75,6 +89,8 @@ cdr::String cdr::addExternalMapping(cdr::Session& session,
     insertStatement.setString(2, value);
     insertStatement.setInt(3, docId);
     insertStatement.setInt(4, session.getUserId());
+    insertStatement.setString(5, bogus);
+    insertStatement.setString(6, mappable);
     insertStatement.executeQuery();
 
     // Report success.
