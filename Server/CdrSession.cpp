2aa7b281258716d3e1377e1e3e322785354ec296
alan <webcommauto@users.noreply.github.com>
2013-04-02 15:01:16 +0000

    <R11592> Changes to support hashed password, password checking, session duplication.

--- Server/CdrSession.cpp
+++ Server/CdrSession.cpp
@@ -36,6 +36,7 @@
  *
  */
 
+#include <ctime>
 #include "CdrSession.h"
 #include "CdrCommand.h"
 #include "CdrDom.h"
@@ -45,9 +46,120 @@
 #include "CdrDbResultSet.h"
 #include "CdrException.h"
 
-void cdr::Session::lookupSession(const cdr::String& sessionId,
-                                 cdr::db::Connection& conn)
-{
+// #include <iostream> // DEBUG
+
+// The password hashing algorithm to try
+static std::string s_hashAlgo = "SHA1";
+
+// This determines if our SQL Server DBMS can use it
+static bool dbHasHash(cdr::db::Connection&, const std::string&);
+
+// See CdrSession.h
+cdr::String cdr::createSessionRecord(
+    const int           userId,
+    const cdr::String&  userName,
+    cdr::db::Connection conn,
+    const cdr::String&  comment
+) {
+    // Double check user ID - though it should be done elsewhere
+    static const char selectQuery[] =
+        "SELECT id, name"
+        " FROM usr "
+        " WHERE id = ? AND expired IS NULL";
+    cdr::db::PreparedStatement select = conn.prepareStatement(selectQuery);
+    select.setInt(1, userId);
+    cdr::db::ResultSet rs = select.executeQuery();
+
+    // Check user exists and not expired
+    if (!rs.next()) {
+        cdr::log::pThreadLog->Write(
+                L"Failed logon attempt (invalid or expired user name) ",
+                L"name: " + userName);
+        throw cdr::Exception(L"Invalid logon credentials");
+    }
+
+    // Confirm exact name match
+    int id             = rs.getInt(1);
+    cdr::String dbName = rs.getString(2);
+    if (userName != dbName) {
+        cdr::log::pThreadLog->Write(
+                L"Failed logon attempt (user name case mismatch)",
+                L"user typed: " + userName + L"; name in database: " + dbName);
+        throw cdr::Exception(L"Invalid logon credentials");
+    }
+    select.close();
+
+    // Create a new row in the session table.
+    char idBuf[256];
+    unsigned long now = (unsigned long)time(0);
+    unsigned long ticks = clock();
+    static char randomChars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    static size_t nRandomChars = sizeof randomChars - 1;
+    srand(ticks);
+    sprintf(idBuf, "%lX-%06lX-%03d-%c%c%c%c%c%c%c%c%c%c%c%c",
+        now, ticks & 0xFFFFFF, id % 1000,
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars],
+        randomChars[rand() % nRandomChars]);
+    cdr::String sessionId = idBuf;
+    static const char insertQuery[] =
+        "INSERT INTO session(name, usr, comment, initiated, last_act)"
+        "     VALUES (?, ?, ?, GETDATE(), GETDATE())";
+    cdr::db::PreparedStatement insert = conn.prepareStatement(insertQuery);
+    insert.setString(1, sessionId);
+    insert.setInt(2, id);
+    insert.setString(3, comment);
+    insert.executeQuery();
+    insert.close();
+
+    return sessionId;
+}
+
+// See CdrSession.h
+cdr::String cdr::duplicateSessionRecord(
+    const cdr::String&   oldSession,
+    cdr::db::Connection& conn,
+    const cdr::String&   comment
+) {
+    int         usrId;
+    cdr::String usrName,
+                newSessionId;
+
+    // Check that existing session exists and is active
+    std::string query =
+        "SELECT s.usr, u.name"
+        "  FROM session s"
+        "  JOIN usr u ON s.usr = u.id"
+        " WHERE s.name = ? AND ended IS NULL";
+    cdr::db::PreparedStatement select = conn.prepareStatement(query);
+    select.setString(1, oldSession);
+    cdr::db::ResultSet rs = select.executeQuery();
+    if (!rs.next())
+        throw cdr::Exception(L"Can't duplicate invalid or expired session",
+                               oldSession);
+    usrId   = rs.getInt(1);
+    usrName = rs.getString(2);
+    select.close();
+
+    // Create and return new session record
+    return createSessionRecord(usrId, usrName, conn, comment);
+}
+
+
+// See CdrSession.h
+void cdr::Session::lookupSession(
+    const cdr::String&   sessionId,
+    cdr::db::Connection& conn
+) {
     std::string query = "SELECT s.id,"
                         "       s.usr,"
                         "       u.name"
@@ -60,7 +172,7 @@ void cdr::Session::lookupSession(const cdr::String& sessionId,
     select.setString(1, sessionId);
     cdr::db::ResultSet rs = select.executeQuery();
     if (!rs.next())
-        throw cdr::Exception(L"Invalid or expired session", sessionId);
+      throw cdr::Exception(L"Invalid or expired session", L"'"+sessionId+L"'");
     name  = sessionId;
     id    = rs.getInt(1);
     uid   = rs.getInt(2);
@@ -106,6 +218,295 @@ bool cdr::Session::canDo(db::Connection& conn,
     int count = rs.getInt(1);
     return count > 0;
 }
+
+
+// See CdrSession.h
+int cdr::chkIdPassword(
+    cdr::String& userName,
+    cdr::String& password,
+    cdr::db::Connection& conn
+) {
+    // Look in the plain text or hashed password
+    bool useHash = dbHasHash(conn, s_hashAlgo);
+
+    // Lookup user info
+    cdr::String chkdPassword(password);
+    std::string query;
+    if (useHash) {
+        // Implementation note:
+        // Hashing single byte characters and double byte characters produces
+        //  different results.  We're doing everything as single byte,
+        //  which obviates use of qmark processing with setString().
+        std::string pw = password.toUtf8();
+        query =
+            "SELECT id, name, hashedpw"
+            "  FROM usr"
+            " WHERE name = ?"
+            "   AND HASHBYTES('" + s_hashAlgo + "', '" + pw + "') = hashedpw";
+    }
+    else {
+        query =
+            "SELECT id, name, password "
+            "  FROM usr "
+            " WHERE name = ?";
+            // " WHERE name = ? AND password = ?";
+    }
+    cdr::db::PreparedStatement select = conn.prepareStatement(query);
+
+    select.setString(1, userName);
+    // select.setString(2, password);
+    cdr::db::ResultSet rs = select.executeQuery();
+    if (!rs.next()) {
+        // No match
+        return -1;
+    }
+
+    // Name matching is case insensitive, make further check here
+    int usrId              = rs.getInt(1);
+    cdr::String dbName     = rs.getString(2);
+    cdr::String dbPassword = rs.getString(3);
+    select.close();
+
+    if (userName != dbName) {
+        cdr::log::pThreadLog->Write(
+                L"Failed logon attempt (user name case mismatch)",
+                L"user typed: " + userName + L"; name in database: " + dbName);
+        return -1;
+    }
+
+    // Check passwords
+    if (!useHash && (chkdPassword != dbPassword)) {
+        // Mismatch on chkdPassword / dbPassword, has to be case difference
+        cdr::log::pThreadLog->Write(L"Failed logon attempt (invalid password)",
+                L"name: " + userName);
+        return -1;
+    }
+
+    // Found it
+    return usrId;
+}
+
+void cdr::testPasswordString(
+    const cdr::String& password
+) {
+    const wchar_t *p;   // Characters in the password
+    int c;              // Single char from the password
+    int hasUpper = 0;   // Characteristics we have found
+    int hasLower = 0;
+    int hasDigit = 0;
+    int hasPunct = 0;
+
+    // Minimum length
+    if (password.length() < 8)
+        throw cdr::Exception("Passwords must be at least 8 characters");
+    if (password.length() > 32)
+        // This is only necessary while we allow plain text pw's
+        throw cdr::Exception(
+                "Sorry, the maximum length of passwords is 32 characters");
+
+    p = password.c_str();
+    while (*p) {
+        c = (int) *p;
+        // Test for legal char types
+        if (iswupper(c))
+            hasUpper = 1;
+        else if (iswlower(c))
+            hasLower = 1;
+        else if (iswdigit(c))
+            hasDigit = 1;
+        else if (iswpunct(c))
+            hasPunct = 1;
+        else
+            throw cdr::Exception("Invalid character in password.  "
+                    "Please only use letters, numbers, and punctuation.");
+
+        // Test for some illegal characters
+        if (c == '\'' || c == '"' || c == ' ')
+            throw cdr::Exception(
+                    "Sorry, quote marks may not be used in CDR passwords");
+        if (c == ' ')
+            throw cdr::Exception(
+                    "Sorry, spaces may not be used in CDR passwords");
+        if (c > 127)
+            throw cdr::Exception(
+                    "Sorry, Unicode chars may not be used in CDR passwords");
+        if (c < 32)
+            // Check above should prevent this but - belt and suspenders
+            throw cdr::Exception("Control chars may not be used in passwords");
+
+        ++p;
+    }
+
+    int sumTypes = hasUpper + hasLower + hasDigit + hasPunct;
+    if (sumTypes < 3)
+        throw cdr::Exception("Passwords must have 3 of the 4 categories of "
+                "upper case, lower case, numbers, or punctuation.");
+
+    return;
+}
+
+void cdr::Session::setUserPw (
+    cdr::db::Connection& conn,
+    const cdr::String&   userName,
+    const cdr::String&   password,
+    const bool           newUser
+) {
+    std::string qry;            // Update query
+
+    // If the session's user isn't the owner of the pw, is he
+    //  allowed to change the owner's password?
+    if (uName != userName) {
+        if ((newUser && !canDo(conn, L"CREATE USER", L"")) ||
+                        !canDo(conn, L"MODIFY USER", L""))
+            throw cdr::Exception(
+                L"User is not authorized to change other user's password");
+    }
+
+    // Check that the password meets security requirements, exception if not
+    // Passwords that have passed checks are SQL injection safe
+    testPasswordString(password);
+
+    // Can we hash the password?
+    if (dbHasHash(conn, s_hashAlgo)) {
+        // Use the std::string rather than wide chars to get straightforward
+        //  hashing - same going in, going out, and if run in stored proc
+        std::ostringstream os;
+        os << "UPDATE usr"
+           << " SET hashedpw = HASHBYTES('"
+           << s_hashAlgo
+           << "', '"
+           << password.toUtf8()
+           << "')"
+           << " WHERE name = ?";
+        qry = os.str();
+
+        cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+        stmt.setString(1, userName);
+        stmt.executeQuery();
+        stmt.close();
+    }
+    // XXX Do this unconditionally until testing is complete
+    // else
+    {
+        qry =
+            "UPDATE usr "
+            "   SET password = ?"
+            " WHERE name = ?";
+        cdr::db::PreparedStatement stmt = conn.prepareStatement(qry);
+        stmt.setString(1, password);
+        stmt.setString(2, userName);
+        stmt.executeQuery();
+        stmt.close();
+    }
+}
+
+
+/**
+ * Does the DBMS support password hashing of a specific type.
+ *
+ * Subroutine of chkIdPassword().
+ *
+ *  @param algo     Check this algorithm, e.g., 'SHA1'
+ *  @param conn     Database connection
+ *
+ *  @return         True = Yes it does.
+ */
+static bool dbHasHash(
+    cdr::db::Connection& conn,
+    const std::string&   algo
+) {
+    static bool tested = false;
+    static bool testresult;
+
+    // Short circuit
+    if (tested)
+        return testresult;
+
+    cdr::String hashed;
+
+    // Query to select hashbytes from passed algorithm name
+    // Can "foo" be hashed by SQL Server using the passed algorithm?
+    std::string qry = "SELECT HASHBYTES('" + algo + "', 'foo')";
+
+    // Assume it works until proven otherwise
+    testresult = true;
+
+    // Test
+    try {
+        cdr::db::PreparedStatement ps = conn.prepareStatement(qry);
+        cdr::db::ResultSet rs = ps.executeQuery();
+        hashed = rs.getString(1);
+        ps.close();
+        if (hashed.isNull() || hashed == L"")
+            // Function is supported but algorithm is not
+            testresult = false;
+    }
+    catch (...) {
+        // HASHBYTES function is unsupported
+        testresult = false;
+    }
+
+    // Don't have to try again
+    tested = true;
+
+    return testresult;
+}
+
+
+int cdr::getLoginFailedCount(
+    cdr::db::Connection& conn,
+    const cdr::String&   userName
+) {
+    int count;
+
+    // Get the count from the user record
+    cdr::db::PreparedStatement ps = conn.prepareStatement(
+            "SELECT login_failed FROM usr WHERE name = ?");
+    ps.setString(1, userName);
+    cdr::db::ResultSet rs = ps.executeQuery();
+    if (rs.next())
+        count = rs.getInt(1);
+    else
+        count = -1;
+    ps.close();
+
+    return count;
+}
+
+
+int cdr::setLoginFailedCount(
+    cdr::db::Connection& conn,
+    const cdr::String&   userName,
+    int                  counter
+) {
+    if (counter != 1 && counter != 0)
+        // This could only be a bug
+        throw cdr::Exception(
+            L"Invalid counter=" + cdr::String::toString(counter) +
+            L" passed to setLoginFailedCount - please inform support staff.");
+
+    // Get current count
+    int oldCount = getLoginFailedCount(conn, userName);
+    if (oldCount == -1)
+        // User not found
+        return oldCount;
+
+    // Generate the new count
+    int newCount = 0;
+    if (counter != 0)
+        newCount = oldCount + 1;
+
+    // Store it
+    cdr::db::PreparedStatement ps = conn.prepareStatement(
+        "UPDATE usr SET login_failed = ? WHERE name = ?");
+    ps.setInt(1, newCount);
+    ps.setString(2, userName);
+    ps.executeQuery();
+    ps.close();
+
+    return newCount;
+}
+
 
 bool cdr::Session::canDo(db::Connection& conn,
                          const cdr::String& action,
