e80e21790f4617c816820054c9ffb6015ff90539
Bob Kline <bkline@rksystems.com>
2013-08-21 21:11:07 +0000

    <R11965> Script to preprocess login creation SQL script for the current tier

---
+++ Database/PrepareCreateLoginsScript.py
@@ -0,0 +1,30 @@
+#----------------------------------------------------------------------
+#
+# $Id$
+#
+# Preprocess login creation/configuration script for this tier.
+#
+#----------------------------------------------------------------------
+import cdrutil, cdrpw, time
+
+script = open("CreateLogins.sql", "rb").read()
+env = cdrutil.getEnvironment()
+tier = cdrutil.getTier()
+dboPw = cdrpw.password(env, tier, "cdr", "CdrSqlAccount")
+guestPw = cdrpw.password(env, tier, "cdr", "CdrGuest")
+pubPw = cdrpw.password(env, tier, "cdr", "CdrPublishing")
+script = script.replace("@@DBOPW@@", dboPw)
+script = script.replace("@@GUESTPW@@", guestPw)
+script = script.replace("@@PUBPW@@", pubPw)
+if tier == "PROD":
+    script = script.replace("@@USRGUESTACCESS@@", "")
+else:
+    script = script.replace("@@USRGUESTACCESS@@", """
+GRANT SELECT ON usr TO CdrGuest
+GO""")
+now = time.strftime("%Y%m%d%H%M%S")
+name = "CreateLogins-%s.sql" % now
+fp = open(name, "wb")
+fp.write(script)
+fp.close()
+print "wrote", name
