5f193a47d93c6f2e9dfe0672ccb0038e48af9e3e
alan <webcommauto@users.noreply.github.com>
2013-08-27 15:32:28 +0000

    <R11974> Modified ctl table and dropped ctl_grp.  The new version supports new features as implemented in CdrCtl.h and CdrCtl.cpp

--- Database/tables.sql
+++ Database/tables.sql
@@ -75,38 +75,31 @@ CREATE TABLE common
 GO
 
 /* 
- * Valid categories for control values.
- *
- * NOTE: Not currently used.
- *
- *           id  primary key for the control group table
- *         name  label used to identify all control values in a given group;
- *               e.g., CNTRY
- *      comment  optional text description of the purpose of the group; e.g.
- *               ISO country code values
- */
-CREATE TABLE ctl_grp
-         (id INTEGER IDENTITY PRIMARY KEY,
-        name VARCHAR(32) NOT NULL UNIQUE,
-     comment VARCHAR(255)    NULL)
-GO
-
-/* 
  * Named and typed values used to control various run-time settings.
  *
- * NOTE: Not currently used.
+ * Software in the CdrServer enforces a constraint that only one grp+name
+ * combination is allowed to be active (i.e., inactivated is null) at a time.
+ * If there is more than one, the software will throw an exception.  An
+ * attempt to add one via the CdrServer software will inactivate any existing
+ * active row with the same grp+name.
  *
- *          grp  identifies which control group this value belongs to
- *         name  mnemonic label for value; e.g., US
- *          val  value associated with this name; e.g., United States
- *      comment  optional text description of the use of this value
+ *           id  unique row ID.
+ *          grp  identifies group this name=value belongs to, e.g. "dbconnect"
+ *         name  mnemonic label for value; e.g., "timeout"
+ *          val  value associated with this name; e.g., "3000"
+ *      comment  optional text description of the use of this value,
+ *                e.g., "Database connection timeout, in milliseconds"
+ *      created  Datetime this row was created
+ *  inactivated  Datetime row made inactive, not used on controlling ops
  */
 CREATE TABLE ctl
-        (grp INTEGER     NOT NULL REFERENCES ctl_grp,
-        name VARCHAR(32) NOT NULL,
-         val NVARCHAR(255)   NULL,
+         (id INTEGER         IDENTITY PRIMARY KEY,
+         grp VARCHAR(255)    NOT NULL,
+        name VARCHAR(255)    NOT NULL,
+         val NVARCHAR(255)   NOT NULL,
      comment VARCHAR(255)    NULL,
- PRIMARY KEY (grp, name))
+     created DATETIME        NOT NULL,
+ inactivated DATETIME        NULL)
 GO
 
 /* 
